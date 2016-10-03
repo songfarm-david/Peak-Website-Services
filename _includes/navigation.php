b6464c9e2878d21608d6977365b691d48aae7a3b
David Gaskin <davidburkegaskin@gmail.com>
2016-10-03 14:30:32 -0700

    Write navigation HTML

---
+++ _includes/navigation.php
@@ -0,0 +1,25 @@
+<nav class="navbar">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+      <span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+    </button>
+	</div>
+	<div class="collapse navbar-collapse" id="main-nav">
+		<ul>
+			<li><a href="about-us.php">About Us</a></li>
+			<li><a href="what-we-do.php">What We Do</a></li>
+			<li class="dropdown-btn">
+				<a href="types-of-signs.php" data-toggle="collapse" data-target=".sub-nav-collapse-1">Types of Signs</a>
+				<div class="collapse navbar-collapse sub-nav-collapse-1">
+					<ul class="dropdown-content">
+						<li><a href="about-us.php">Sign Type A</a></li>
+						<li><a href="about-us.php">Sign Type B</a></li>
+						<li><a href="about-us.php">Sign Type C</a></li>
+						<li><a href="about-us.php">Sign Type D</a></li>
+					</ul>
+				</div>
+			</li>
+			<li><a href="contact-us.php">Contact Us</a></li>
+		</ul>
+	</div>
+</nav>
