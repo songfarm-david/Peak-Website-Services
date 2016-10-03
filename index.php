c2e030ec9cbb01fdc55e63a074d13f5cca37935c
David Gaskin <davidburkegaskin@gmail.com>
2016-10-03 16:23:08 -0700

    Add some basic layout styles for Triad

--- index.php
+++ index.php
@@ -1,67 +1,15 @@
-<!DOCTYPE html>
-<!--[if IE 8]><html class="ie8 no-js"><![endif]-->
-<!--[if gt IE 8]><!-->
-<html lang="en" class="no-js">
-<!--<![endif]-->
-	<head>
-		<!-- Detects and modifies html class 'no-js' if JavaScript is enabled -->
-		<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
-		<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Triad Sign</title>
-		<meta name="description" content="">
-		<link rel="apple-touch-icon" href="apple-touch-icon.png">
-		<link rel="stylesheet" href="_css/main.css" media="screen" title="main page styles">
-	</head>
-	<body>
-		<!--[if lt IE 8]>
-				<script src="_js/vendor/html5shiv.js"></script>
-				<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-		<![endif]-->
-		<h1 class="hide">Welcome to Triad Sign</h1>
+<?php require_once('_includes/head.php') ?>
+<h1 class="hide">Welcome to Triad Sign</h1>
 
 			<!-- Content Here -->
 		<header id="prime-header">
-
-			<img src="logo-here" alt="" />
+			<div><img src="_images/demo-triad-logo.jpg" alt="" height="91" width="129"/></div>
 			<?php include_once('_includes/navigation.php') ?>
-
 		</header>
 
 		<main>
 			<section id="case-studies">
-				<h2></h2>
 			</section>
 		</main>
 
-
-		<footer>
-
-		</footer>
-
-	</body>
-	<!--jQuery CDN: NOTE: continue to update 3rd party reliances -->
-	<script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
-	<!-- Fallback for jQuery CDN -->
-	<script type="javascript">window.jQuery || Modernizr.addTest('hasJquery', 'jQuery' in window); || document.write('<script src="_js/vendor/jquery-3.1.0.slim.min.js"><\/script>')</script>
-	<!-- Minified custom Modernizr v.3.3.1 - detects browser features -->
-	<script type="javascript" src="_js/vendor/modernizr.js"></script>
-	<!-- JS Helper Functions -->
-	<script src="_js/vendor/plugins.js"></script>
-	<!-- supports media queries in IE6-8 -->
-	<!--[if lte 8]>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
-	<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-	<script>
-			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-			function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-			e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-			e.src='https://www.google-analytics.com/analytics.js';
-			r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-			ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-	</script>
-	<!-- http://www.paulirish.com/2009/avoiding-the-fouc-v3/ -->
-	<!-- http://www.paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-</html>
+		<?php require_once('_includes/footer.php') ?>
