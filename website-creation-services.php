b4cf6e2bf8e45bb328dc31bef9fc1d20d42868f8
David Gaskin <davidburkegaskin@gmail.com>
2016-10-16 12:56:01 -0700

    Update optimization services; Create template for Creation services

---
+++ website-creation-services.php
@@ -0,0 +1,7 @@
+<?php require_once('_includes/header.php') ?>
+
+<h1>Website Creation Service</h1>
+
+
+
+<?php require_once('_includes/footer.php') ?>
--- website-optimization-service.php
+++ website-optimization-service.php
@@ -2,9 +2,17 @@
 
 <h1>Website Optimization Service</h1>
 
-Search Engine Optimization and knowing how to create websites that Search Engines can understand
-Rely on Best Practices
-I want to convey that there site will follow Search Engine recommended guidelines.
-
+<p>
+	Anyone who's serious about the success of their online business needs to understand the importance of optimizing their website for site visitors and the search engines. An optimized website not only improves your visibility on the search engine result pages but improves the user experience for your site visitors, which ultimately leads to higher levels of success.
+</p>
+<p>
+	Search Engine Optimization, or SEO as it's commonly referred to in the industry, is the ongoing process of remaining visible in the search engine result pages while simultaneously and consistently connecting to your target audience, those who drive your business farther and allow you to get closer to reaching your business goals.
+</p>
+<p>
+	Let Peak Creative guide you towards full site optimization. We begin
+</p>
+<p>
+	Have your online business working <em>for you</em>.
+</p>
 
 <?php require_once('_includes/footer.php') ?>
