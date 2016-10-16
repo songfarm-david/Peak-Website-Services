a058d67dd79b476ef144e7e3623d3e91fd955a1d
David Gaskin <davidburkegaskin@gmail.com>
2016-10-15 23:10:52 -0700

    Create placeholder page for website optimization

---
+++ website-optimization-service.php
@@ -0,0 +1,10 @@
+<?php require_once('_includes/header.php') ?>
+
+<h1>Website Optimization Service</h1>
+
+Search Engine Optimization and knowing how to create websites that Search Engines can understand
+Rely on Best Practices
+I want to convey that there site will follow Search Engine recommended guidelines.
+
+
+<?php require_once('_includes/footer.php') ?>
