902523236be4b164b9902e067867ea26fa25891f
David Gaskin <davidburkegaskin@gmail.com>
2016-10-16 16:42:37 -0700

    Begin forming copy for website creation services

--- website-creation-services.php
+++ website-creation-services.php
@@ -2,14 +2,44 @@
 
 <h1>Website Creation Service</h1>
 
+<section>
+	<h2>Do I need a website if I do business online?</h2>
+	<!-- <p>
+		If you do business online, or plan on having your own online business, there's no underestimating the importance of having an effective website. In many cases, your website is where site visitors become paying customers.
+	</p> -->
+	<!-- <p>
+		If you do business online, or plan on having your own online business, having a website is an essential component to any kind of online success. Aside from representing your values and championing your brand, your website is key to converting site visitors into paying customers.
+	</p> -->
+	<p>
+		If you do business online, or plan on having your own online business, having a website is essential to online success. Apart from representing your company values and championing your brand, a solid and effective website is critical in converting site visitors into paying customers.
+	</p>
+	<p>
+		If you do business online, or plan on having your own online business, having a website is essential to online success. Apart from representing your online presence, an effective website is critical in converting site visitors into paying customers.
+	</p>
+	<p>
+		So whats the difference between an effective website &mdash; one that works <em>for</em> you &mdash; and a not-so-effective one?
+	</p>
+	<p>
+		At Peak Creative, we believe that a website should have a clear purpose, and although that purpose differs from business to business, the measure of an effective website is how well it fulfills its purpose to your business. That purpose, which we discover early on/first in the website creation process, guides our decisions going forward.
+	</p>
+	<blockquote cite="http://">
+		"The purpose of having a website is to convert site visitors into paying customers" (can search 'what is the purpose of having a website or smthg')
+	</blockquote>
+</section>
+
+
+
 <p>
-	There's no denying the fact that websites and online businesses are everywhere these days. But just how effective is your web presence? Surprisingly, the vast majority of websites under-perform, are under-maintained, deter potential customers, and perhaps worst of all, serve no measurable purpose to the business.
+	There's no denying the fact that... are everywhere these days. But just how effective is your web presence? Surprisingly, the vast majority of websites today under-perform, are under-maintained, deter potential customers, and perhaps worst of all, serve no measurable purpose to the business.
+</p>
+<p>
+	There's no denying the importance of a website, but just how effective is your web presence? Perhaps not so surprisingly, the vast majority of websites today fail to fulfill the basic needs of the business. (Need reference). Whether that's promoting an event, providing information, or converting a sale, many websites fail to do
 </p>
 <p>
 	We don't play that game. We strongly believe that a website is a tool and it should be created and designed to perform a particular function. At the end of the day, it should support your efforts and serve your business outcomes.
 </p>
 <p>
-	We know first hand what goes into creating a website and getting it off the ground. From content writing and brand creation to website hosting and maintenance services, Peak Creative Website Services provides everything you need to establish a purposeful and effective web presence online.
+	We know first hand what goes into creating a website and getting it off the ground. From content writing and brand creation to website hosting and maintenance services, Peak Creative gives you everything you need to create, establish, and maintain a purposeful and effective web presence online.
 </p>
 <p>
 	Find out how affordable it is to Create a Website for your business (to pricing)?
