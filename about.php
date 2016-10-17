db94c95e8abab9edabc933b26f8aefe4cd9dd394
David Gaskin <davidburkegaskin@gmail.com>
2016-10-16 20:55:41 -0700

    Add copy for index.php display page, about page

---
+++ about.php
@@ -0,0 +1,18 @@
+<?php require_once('_includes/header.php') ?>
+
+		<main>
+
+			<a href="#">
+				Leave the technical work of building and maintaining your website to us.
+			</a>
+			<p>
+				Peak Website Services is a complete solution to business owners who are not tech-savvy and would rather run their business than worry about code.
+			</p>
+			<p>
+				Our professional developers and designers combine website and brand creation, website development and maintenance, and strategy-driven website optimization into affordable website services designed to maximize your online presence and make handling the technical aspects of having an online business a breeze.
+			</p>
+
+
+		</main>
+
+		<?php require_once('_includes/footer.php') ?>
--- index.php
+++ index.php
@@ -6,58 +6,14 @@
 				<div class="cta">
 					<img src="" alt="Focus shot of a large, snow-covered mountain peak" />
 					<span class="cta ">Put Your Business On Top</span>
-
-					<span>Let us take the worry out of your online business.</span>
-					<!-- <p>
-						You know how important having the <em>right</em> website is to online success.
-						Do it right. Let Peak Website Services take the worry out of your online business.
-					</p> -->
 					<p>
 						You know how important having the <em>right</em> website is to online success.
-						Our professional developers and designers make your online success an affordable reality.
-						<a href="#">Read more...</a>
+						Our professional developers and designers make (your) online success an affordable reality.
+						<a href="about.php">Read more...</a>
 					</p>
 					<a href="#">See Website Services</a>
 				</div>
 			</section>
-			<!-- <section>
-				<h2>Case Studies</h2>
-				<p>
-					Need more information? Learn from these <a href="/">useful case studies</a>.
-				</p>
-			</section> -->
-			<a href="#">Leave the technical work of building and maintaining your website to us.</a>
-			<a href="#">Leave the technical work of building and maintaining your website to us.</a>
-
-
-			<!-- Testimonials here -->
-
-			<span class="header">
-				We take the worry/work out of your online business.
-			</span>
-			<!-- <p>
-				Peak Website Services is a complete solution to business owners who are not tech-savvy and would rather hire a professional to create, maintain or optimize their website.
-			</p> -->
-			<p>
-				Peak Website Services is a complete solution to business owners who are not tech-savvy and would rather run their business than worry about code.
-			</p>
-			<!-- <p>
-				We are a business solution strategy that combines website and brand creation, website development and maintenance, and strategy-driven website optimization into an affordable and effective strategy for business owners who would rather run their business then worry about code.
-			</p> -->
-			<p>
-				Our professional developers and designers combine website and brand creation, website development and maintenance, and strategy-driven website optimization into affordable website services designed to maximize your search engine visibility and improve .
-			</p>
-			<p>
-				From new custom designed websites or remodelling your existing one to personal branding and corporate branding creation services, <a href="">web development and maintenance services</a>, and website optimization services to maximize your search engine visibility and improve your targeted traffic.
-			</p>
-			<p>
-				We are the complete solution to your online business.
-			</p>
-			<p>
-				Creation. Maintenance. Optimization.
-			</p>
-
-
 		</main>
 
 		<?php require_once('_includes/footer.php') ?>
