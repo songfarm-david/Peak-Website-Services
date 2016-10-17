8e843bc5df22e5f34cd449c0500b57a787e0c636
David Gaskin <davidburkegaskin@gmail.com>
2016-10-17 13:24:01 -0700

    Create full-size landing page with Mountain Image

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php require_once('_includes/header.php') ?>
 
-		<main>
-			<section>
-				<h1>Peak Website Services</h1>
-				<div class="cta">
-					<img src="" alt="Focus shot of a large, snow-covered mountain peak" />
-					<span class="cta ">Put Your Business On Top</span>
-					<p>
-						You know how important having the <em>right</em> website is to success online.
-						Our professional website developers and designers make online success an affordable reality.
-						<a href="about.php">Read more...</a>
-					</p>
-					<a href="#">See Website Services</a>
-				</div>
-			</section>
-			<section>
+	<?php include_once('_includes/navigation.php') ?>
+
+	<section id="landing-page-intro">
+		<h1 class="hide">Peak Website Services</h1>
+		<img class="hide" src="" alt="Focus shot of a large, snow-covered mountain peak" />
+		<span class="">Put Your Business On Top</span>
+		<p>
+			You know how important having the <em>right</em> website is to success online.
+			Our professional website developers and designers make online success an affordable reality.
+			<a href="../about.php">Read more</a>
+		</p>
+		<a href="#">See website services</a>
+	</section>
+
+			<!-- Website Services -->
+			<!-- <section>
 				<h2>Website Services</h2>
-			</section>
-		</main>
+			</section> -->
+
+			<!-- Core Values -->
+
+			<!-- Design Process -->
+			<!-- <section>
+				<h2>The Website Creation Process</h2>
+			</section> -->
 
 		<?php require_once('_includes/footer.php') ?>
