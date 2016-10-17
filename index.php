b5cb3e162066b52de42c19bdf7c7e7eeafd11ce6
David Gaskin <davidburkegaskin@gmail.com>
2016-10-16 20:41:46 -0700

    Rejig main about copy

--- index.php
+++ index.php
@@ -14,11 +14,9 @@
 					</p> -->
 					<p>
 						You know how important having the <em>right</em> website is to online success.
-						Our professional developers and designers make online success an affordable reality.
+						Our professional developers and designers make your online success an affordable reality.
 						<a href="#">Read more...</a>
-						Peak Website Services take the worry out of your online business.
 					</p>
-					<a href="">Read More</a>
 					<a href="#">See Website Services</a>
 				</div>
 			</section>
@@ -29,9 +27,35 @@
 				</p>
 			</section> -->
 			<a href="#">Leave the technical work of building and maintaining your website to us.</a>
+			<a href="#">Leave the technical work of building and maintaining your website to us.</a>
 
 
 			<!-- Testimonials here -->
+
+			<span class="header">
+				We take the worry/work out of your online business.
+			</span>
+			<!-- <p>
+				Peak Website Services is a complete solution to business owners who are not tech-savvy and would rather hire a professional to create, maintain or optimize their website.
+			</p> -->
+			<p>
+				Peak Website Services is a complete solution to business owners who are not tech-savvy and would rather run their business than worry about code.
+			</p>
+			<!-- <p>
+				We are a business solution strategy that combines website and brand creation, website development and maintenance, and strategy-driven website optimization into an affordable and effective strategy for business owners who would rather run their business then worry about code.
+			</p> -->
+			<p>
+				Our professional developers and designers combine website and brand creation, website development and maintenance, and strategy-driven website optimization into affordable website services designed to maximize your search engine visibility and improve .
+			</p>
+			<p>
+				From new custom designed websites or remodelling your existing one to personal branding and corporate branding creation services, <a href="">web development and maintenance services</a>, and website optimization services to maximize your search engine visibility and improve your targeted traffic.
+			</p>
+			<p>
+				We are the complete solution to your online business.
+			</p>
+			<p>
+				Creation. Maintenance. Optimization.
+			</p>
 
 
 		</main>
