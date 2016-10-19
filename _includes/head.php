ea825368e4ce65ad43a9d8c264e429fb976e8dd9
David Gaskin <davidburkegaskin@gmail.com>
2016-10-19 12:46:22 -0700

    update page title accordingly

--- _includes/head.php
+++ _includes/head.php
@@ -9,7 +9,7 @@
 		<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Triad Sign</title>
+		<title></title>
 		<meta name="description" content="">
 		<link rel="apple-touch-icon" href="apple-touch-icon.png">
 		<link rel="stylesheet" href="_css/main.min.css" media="screen" title="main page styles">
