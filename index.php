c33e1da261a10a62ae37b19488c7d41fec823d13
David Gaskin <davidburkegaskin@gmail.com>
2016-10-19 10:43:23 -0700

    Write mark-up for footer; Modify footer include

--- index.php
+++ index.php
@@ -1,33 +1,85 @@
 <?php require_once('_includes/header.php') ?>
 
 <header>
-	<h1 class="h3">Peak Website Services</h1>
+	<!-- Logo Here -->
 	<?php include_once('_includes/navigation.php') ?>
-
 </header>
 
-	<section id="landing-page-intro">
-		<h1 class="hide">Peak Website Services</h1>
-		<img class="hide" src="" alt="Focus shot of a large, snow-covered mountain peak" />
-		<span class="">Put Your Business On Top</span>
-		<p>
-			You know how important having the <em>right</em> website is to success online.
-			Our professional website developers and designers make online success an affordable reality.
-			<a href="../about.php">Read more</a>
-		</p>
-		<a href="#">See website services</a>
-	</section>
+	<!-- carousel slider here -->
+	<div id="carousel-slider">
 
-			<!-- Website Services -->
-			<!-- <section>
-				<h2>Website Services</h2>
-			</section> -->
+		<article>
+			<h3>Put Your Business On Top</h3>
+			<p>
+				You know how important having the <em>right</em> website is to success online.
+				Our professional website developers and designers make online success an affordable reality.
+				<a href="../about.php">Read more</a>
+			</p>
+			<a href="#">See website services</a>
+		</article>
+		<article>
+			<h3>Put Your Business On Top</h3>
+			<p>
+				You know how important having the <em>right</em> website is to success online.
+				Our professional website developers and designers make online success an affordable reality.
+				<a href="../about.php">Read more</a>
+			</p>
+			<a href="#">See website services</a>
+		</article>
+		<article>
+			<h3>Put Your Business On Top</h3>
+			<p>
+				You know how important having the <em>right</em> website is to success online.
+				Our professional website developers and designers make online success an affordable reality.
+				<a href="../about.php">Read more</a>
+			</p>
+			<a href="#">See website services</a>
+		</article>
 
-			<!-- Core Values -->
+	</div>
+	<!-- end of carousel slider -->
 
-			<!-- Design Process -->
-			<!-- <section>
-				<h2>The Website Creation Process</h2>
-			</section> -->
 
-		<?php require_once('_includes/footer.php') ?>
+
+	<!-- footer -->
+	<footer>
+
+		<!-- address -->
+		<section>
+			<h4 class="hide">Address and Contact Information</h4>
+			<div id="address-container">
+				<span class="address">P.O Box V5N 1P9</span>
+				<span class="address">123 Granville Street, Suite 700</span>
+				<span class="address">Victoria, BC V7Y 1G5</span>
+				<!-- phone contact -->
+				<p>
+					Contact Us: <a href="tel:1234567890" class="contact contact-phone">123-456-7890</a>
+				</p>
+			</div>
+		</section>
+
+		<!-- mission statement -->
+		<section>
+			<h4>Mission Statement</h4>
+			<p>
+				Hollis Wealth is a tart icing fruitcake gummies marshmallow dessert soufflé chupa chups. Oat cake cookie pudding. Fruitcake marshmallow chupa chups donut sweet sugar plum muffin jujubes cheesecake. Cake chupa chups I love toffee powder jelly marshmallow. Jujubes I love marshmallow tootsie roll marshmallow I love donut. Chocolate cake cookie cake cupcake gingerbread.
+			</p>
+		</section>
+
+		<!-- disclaimer -->
+		<section>
+			<h4>Disclaimer</h4>
+			<small>
+				Jelly beans ice cream powder carrot cake topping I love danish. Chocolate I love liquorice candy sweet roll sesame snaps. Wafer lollipop jelly beans donut I love chocolate cake cake marzipan. Biscuit danish pudding apple pie halvah danish marshmallow apple pie bonbon. Bonbon biscuit lollipop tart sesame snaps lollipop tart jelly beans. Gingerbread jelly fruitcake powder wafer dessert I love. Lemon drops liquorice jelly. Tart cake bear claw. Sweet roll oat cake sweet I love dragée I love sugar plum I love pudding. Lemon drops wafer pastry gummies ice cream wafer soufflé oat cake marzipan.
+			</small>
+		</section>
+
+		<!-- copyright -->
+		<section>
+			<h4 class="hide">Copyright</h4>
+			<small>Copyright &copy; <?php echo date("Y"); ?> Peak Website Services LLC.</small>
+		</section>
+
+	</footer>
+
+	<?php require_once('_includes/foot.php') ?>
