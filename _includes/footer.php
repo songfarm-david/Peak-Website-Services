47d60bd356915b739f9523bf6857400aab868164
David Gaskin <davidburkegaskin@gmail.com>
2016-10-23 11:20:37 -0700

    combine footer include into just one included file

--- _includes/footer.php
+++ _includes/footer.php
@@ -33,4 +33,31 @@
 	</div>
 </footer>
 
-<?php require_once('_includes/foot.php') ?>
+</body>
+<script async="true" src="https://use.fontawesome.com/85510088e9.js"></script>
+<!--jQuery CDN: NOTE: continue to update 3rd party reliances -->
+<script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
+<!-- Fallback for jQuery CDN -->
+<script type="javascript">window.jQuery || Modernizr.addTest('hasJquery', 'jQuery' in window); || document.write('<script src="_js/vendor/jquery-3.1.0.slim.min.js"><\/script>')</script>
+<!-- Minified custom Modernizr v.3.3.1 - detects browser features -->
+<script type="javascript" src="_js/vendor/modernizr.js"></script>
+<!-- Bootstrap Collapse script -->
+<script type="text/javascript" src="_js/helper/bs-collapse.js"></script>
+<!-- JS Helper Functions -->
+<!-- <script src="_js/vendor/plugins.js"></script> -->
+<!-- supports media queries in IE6-8 -->
+<!--[if lte 8]>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+		e.src='https://www.google-analytics.com/analytics.js';
+		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+		ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+</script>
+<!-- http://www.paulirish.com/2009/avoiding-the-fouc-v3/ -->
+<!-- http://www.paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+</html>
