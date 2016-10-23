816f4d7e77fd57e56cb8a91e8762476114529ca1
David Gaskin <davidburkegaskin@gmail.com>
2016-10-23 13:13:54 -0700

    Add Google Analytics for Peak Websites

--- _includes/footer.php
+++ _includes/footer.php
@@ -49,14 +49,14 @@
 <!--[if lte 8]>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<!-- Google Analytics ID tracking since 10/23/2016. -->
 <script>
 		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
 		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
 		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
 		e.src='https://www.google-analytics.com/analytics.js';
 		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-		ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+		ga('create','UA-86141289-1','auto');ga('send','pageview');
 </script>
 <!-- http://www.paulirish.com/2009/avoiding-the-fouc-v3/ -->
 <!-- http://www.paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
