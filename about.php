4792e6176e72ea2b5bfea4da906a4433c3717db0
David Gaskin <davidburkegaskin@gmail.com>
2016-10-22 17:04:17 -0700

    about page with great styles

--- about.php
+++ about.php
@@ -1,28 +1,32 @@
 <?php require_once('_includes/header.php') ?>
+
 <header>
-	<h1 class="h3">About Peak Website Services</h1>
+	<h1 class="h3 hide">Peak Website Services</h1>
 	<?php include_once('_includes/navigation.php') ?>
 </header>
 
 	<main>
 
 		<div id="about">
-			<h2>About our services</h2>
-			<span>
-				Leave the technical work of building and maintaining a website to us.
-			</span>
-			<p>
-				Peak Website Services is a complete solution to business owners who are not tech-savvy and would rather run their business than worry about writing code.
-			</p>
-			<p>
-				Our professional developers and designers combine website and brand creation, website development and maintenance, and strategy-driven website optimization into affordable website services designed to maximize your online success and make handling the technical aspects of having an online business simple.
-			</p>
-			<a href="website-services/">
-				Read more about our Website Services
-			</a>
-			<a href="#">
-				View Pricing
-			</a>
+			<h2 class="hide">About our services</h2>
+			<div class="container">
+				<span>
+					<span class="spin">Leave the</span> technical <span class="spin">work</span> of building and maintaining a website <span class="spin">to us</span>.
+				</span>
+				<p>
+					Peak Website Services is a complete solution to business owners who are not tech-savvy and would rather run their business than worry about writing code.
+				</p>
+				<p>
+					Our professional developers and designers combine website and brand creation, website development and maintenance, and strategy-driven website optimization into affordable website services designed to maximize your online success and make handling the technical aspects of having an online business simple.
+				</p>
+				<a href="website-services/">
+					Read more about our Website Services
+				</a>
+				<a href="#">
+					View Pricing
+				</a>
+			</div>
+
 		</div>
 
 	</main>
