202aaa1ac82a3a79cda5841f52eb8e64a513f3b9
David Gaskin <davidburkegaskin@gmail.com>
2016-10-26 10:41:41 -0700

    create FAQ page and add first content

---
+++ faq.php
@@ -0,0 +1,6 @@
+<dl class="">
+	<dt>Why are Canadian-Hosted Websites better than websites hosted in the United States?</dt>
+	<dd><em>Better</em> may not be the best word to describe it, but if you're at all concerned about the privacy of the information that is on your website, or collected through your website and stored (perhaps in a database), then a Canadian-hosted website is the only way to go. </dd>
+</dl>
+<a href="https://en.wikipedia.org/wiki/Patriot_Act">Learn more about US Privacy Laws</a>
+<a href="https://en.wikipedia.org/wiki/Privacy_Act_(Canada)">Learn more about Canadian Privacy Laws</a>
