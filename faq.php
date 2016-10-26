463b1123c71e6aa0c1cf1d83a4ddf708b9d4a0b3
David Gaskin <davidburkegaskin@gmail.com>
2016-10-26 12:33:34 -0700

    commit half-done faq page

--- faq.php
+++ faq.php
@@ -1,6 +1,22 @@
 <dl class="">
-	<dt>Why are Canadian-Hosted Websites better than websites hosted in the United States?</dt>
+	<dt id="canadian-hosting">Why are Canadian-Hosted Websites better than websites hosted in the United States?</dt>
 	<dd><em>Better</em> may not be the best word to describe it, but if you're at all concerned about the privacy of the information that is on your website, or collected through your website and stored (perhaps in a database), then a Canadian-hosted website is the only way to go. </dd>
+	<dt id="existing-host">My website is already being hosted somewhere</dt>
+	<dd>
+		If your website is already being hosted elsewhere, we recommend <a href="faq.php#migrate-a-website">migrating your website</a> to our hosting datacenter. We recommend this to allow us at Peak the ability to optimize your maintenance service experience. If we don't have access to your website or host servers, our services are greatly limited and we may not be able to help you the way we would like.
+	</dd>
+	<dt id="migrate-a-website">What does it mean to migrate a Website?</dt>
+	<dd>
+		The same way birds will migrate from one hemisphere to the other, sometimes a website must be migrated from one datacenter (host) to another. This is usually a simple process, however sometimes it requires a little more finesse. At Peak Website Services we will do the migration for you (usually free of charge barring any unforeseen complications). <a href="contact.php">Contact Us</a> if you require your website be migrated.
+	</dd>
+	<dt id="monthly-website-health-report">What can I expect to see in a Monthly Website Health Report</dt>
+	<dd>
+		As a customer of our Website Maintenance Service Plan, you will receive monthly website health reports that will brief you on ...
+	</dd>
+	<dt id="domain-name">What is a Domain Name</dt>
+	<dd>
+		Quite simply, a domain name is ... . Domain names are registered and need to be re-registered ... 
+	</dd>
 </dl>
 <a href="https://en.wikipedia.org/wiki/Patriot_Act">Learn more about US Privacy Laws</a>
 <a href="https://en.wikipedia.org/wiki/Privacy_Act_(Canada)">Learn more about Canadian Privacy Laws</a>
