3166164ab22332c1a13aa1c8580149ca4097b05d
David Gaskin <davidburkegaskin@gmail.com>
2016-10-27 13:27:33 -0700

    write and base styles new section: why choose us index.php

--- index.php
+++ index.php
@@ -1,23 +1,22 @@
 <?php
 $title = "Peak Website Services";
 $description = "";
+$h1 = $title;
+$h2 = "";
 ?>
 <?php require_once('_includes/header.php') ?>
 
-	<header>
-		<h1 class="h3 hide">Peak Website Services</h1>
-		<?php include_once('_includes/navigation.php') ?>
-	</header>
-
 	<section id="landing-page">
-		<h2 class="hide">Peak Website Services</h2>
+		<h2 class="hide">What We Do</h2>
 		<img class="hide" src="" alt="Focus shot of a large, snow-covered mountain peak" />
 		<div id="landing-page-intro">
 			<span class="">Put Your Business<br> On <span class="yellow">Top</span></span>
 			<div class="">
 				<p>
-					Peak Website Services is a complete solution for non-technical business owners looking to get a foothold into the internet.
-					Learn about our <a href="/peak/website-services/">Website Services</a>
+					Peak Website Services is a technical service company for business owners looking to make their mark on the internet.
+				</p>
+				<p>
+					See our <a href="/peak/website-services/">Website Services</a>
 				</p>
 			</div>
 		</div>
@@ -27,14 +26,35 @@ $description = "";
 	<section id="about" class="container">
 		<h2 class="hide">Why Choose Us</h2>
 		<p>
-			There are a lot of web developers and designers out there, so..<br> <span class="h2">why work with us?</span>
+			There are a lot of web companies out there, so..<br> <span class="h2">why work with us?</span>
 		</p>
-		<!-- <p>
-			Peak Website Services is a team of professional web developers and designers steeped in the language of the web. We  communicate your vision to the digital screen and never lose site of the big picture, which is to optimizing your online presence so that you reach your personal or business goals.
-		</p>
-		<p>
-			We don't quit until the job is done and you are satisfied.
-		</p> -->
+		<ul>
+			<li>Personal Service and Attention
+				<p>
+					Choosing a smaller client-base means we're always just a phone call away. You can think of us as your personal IT guy.
+				</p>
+			</li>
+			<li>Translate your Vision into Beautiful Reality
+				<p>
+					We balance creativity with industry best practices to deliver high-quality products that reflect the uniqueness of your business.
+				</p>
+			</li>
+			<li>Attention to Detail
+				<p>
+					Because it's the little things that make the biggest difference, quality always come before quantity.
+				</p>
+			</li>
+			<li>Eye on the Bigger Picture
+				<p>
+					While there are many steps along the way, we always keep top of mind your business goals.
+				</p>
+			</li>
+			<li>Guaranteed Satisfaction
+				<p>
+					We go the extra mile because your satisfaction is our number one priority.
+				</p>
+			</li>
+		</ul>
 		<aside id="guarantee">
 			<h3>100% Satisfaction Guarantee</h3>
 			<p>
@@ -43,37 +63,29 @@ $description = "";
 		</aside>
 		<!-- <a href="/peak/website-services/index.php" class="cta-link">See Website Services</a> -->
 
-		<p>
+		<!-- <p>
 			Peak Website Services is a one-stop solution for non-technical business owners looking to get a foothold onto the internet.
-		</p>
-		<p>
+		</p> -->
+		<!-- <p>
 			We make having an effective online presence easy and affordable. Best of all, we keep you in the loop so you can always stay focused on the bigger picture, which is running your business.
-		</p>
+		</p> -->
 		<blockquote cite="http://">
 			Think of us as your personal IT team.
 		</blockquote>
-		<p>
+		<!-- <p>
 			There's a lot that goes into having an effective online business. Whether you need a new website or want to give your old one an existing one, We make online success an affordable reality and we always stand behind our work 100%.
-		</p>
-		<p>
+		</p> -->
+		<!-- <p>
 			The team at Peak is made up of professional web developers and designers who have been steeped in the language of the web. We've committed our selves to be peak performers in the internet field so you can focus on more important like running your business.
-		</p>
-		<p>
+		</p> -->
+		<!-- <p>
 			Complete a <a href="#">Free Website Assessment Questionnaire</a> or learn more about our <a href="/peak/website-services/">Website Services</a>
-		</p>
+		</p> -->
 
 	</section>
 
-	<section style="height:100vh;">
-		<h2>What we do/What we can make</h2>
-	</section>
 
-	<!-- Design Process -->
-	<!-- <article id="website-creation-process">
-		<h2>The Website Creation Process</h2>
-		// diagram here
-		<img src="" alt="" />
-	</article> -->
+
 
 
 		<?php require_once('_includes/footer.php') ?>
