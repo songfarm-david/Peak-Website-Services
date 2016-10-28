af412adf1f6212f39465d3220be246f1d2bb131d
David Gaskin <davidburkegaskin@gmail.com>
2016-10-28 12:51:55 -0700

    improve about section, index.php; refactor landing page intro markup

--- index.php
+++ index.php
@@ -10,57 +10,63 @@ $h2 = "";
 		<h2 class="hide">What We Do</h2>
 		<img class="hide" src="" alt="Focus shot of a large, snow-covered mountain peak" />
 		<div id="landing-page-intro" class="container">
-			<span>Put Your Business<br> On <span class="accent-color">Top</span></span>
+			<span id="headline" class="h1">Put Your Business<br> On <span class="accent-color">Top</span></span>
 			<div>
 				<p>
 					Website Services for business owners looking to elevate their online potential.
 				</p>
 				<p>
-					<a href="/peak/website-services/">Learn about our Website Services Plans</a>
+					<a href="/peak/website-services/" class="cta-link">Learn about our Website Services Plans</a>
 				</p>
 			</div>
 		</div>
 	</section>
 
 	<!-- Core Values -->
-	<section id="about" class="container">
+	<section id="about">
 		<h2 class="hide">Why Choose Us</h2>
-		<div class="row">
-			<div class="col col-6">
-				<p>
-					There are a lot of web companies out there, so..<br> <span class="h2">why work with us?</span>
-				</p>
-			</div>
-			<div class="col col-6">
-				<ul>
-					<li>Personal Service and Attention
-						<p>
-							Choosing a smaller client-base means we're always just a phone call away. You can think of us as your personal IT guy.
-						</p>
-					</li>
-					<li>Translate your Vision into Beautiful Reality
-						<p>
-							We balance creativity with industry best practices to deliver high-quality products that reflect the uniqueness of your business.
-						</p>
-					</li>
-					<li>Attention to Detail
-						<p>
-							Because it's the little things that make the biggest difference, quality always come before quantity.
-						</p>
-					</li>
-					<li>Eye on the Bigger Picture
-						<p>
-							While there are many steps along the way, we always keep top of mind your business goals.
-						</p>
-					</li>
-					<li>Guaranteed Satisfaction
-						<p>
-							We go the extra mile because your satisfaction is our number one priority.
-						</p>
-					</li>
-				</ul>
+		<div class="container">
+			<div class="row">
+				<div class="col col-6">
+					<p id="about-intro">
+						There are a lot of web companies out there, so..<br> <span class="h2">why work with us?</span>
+					</p>
+					<a href="/peak/about-peak.php" class="cta-link">Read more about Peak</a>
+				</div>
+				<div class="col col-6">
+					<ul>
+						<li>Eye on the Bigger Picture
+							<p>
+								While there are many steps along the way, we always keep top of mind your business goals.
+							</p>
+						</li>
+						<li>Attention to Detail
+							<p>
+								Because it's the little things that make the biggest difference, quality always come before quantity.
+							</p>
+						</li>
+						<li>Personal Service and Attention
+							<p>
+								Choosing a smaller client-base means we're always just a phone call away. You can think of us as your personal IT guy.
+							</p>
+						</li>
+						<li>Translate your Vision into Beautiful Reality
+							<p>
+								We balance creativity with industry best practices to deliver high-quality products that reflect the uniqueness of your business.
+							</p>
+						</li>
+
+
+						<!-- <li>Guaranteed Satisfaction
+							<p>
+								We go the extra mile because your satisfaction is our number one priority.
+							</p>
+						</li> -->
+					</ul>
+				</div>
 			</div>
 		</div>
+
 
 
 		<!-- <aside id="guarantee">
