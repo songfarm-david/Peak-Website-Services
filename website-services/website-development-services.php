52ae1181f93faa4649c45611102c2dd4f272a0fd
David Gaskin <davidburkegaskin@gmail.com>
2016-10-28 09:58:51 -0700

    clean up website dev services page

--- website-services/website-development-services.php
+++ website-services/website-development-services.php
@@ -38,92 +38,45 @@ $activePage = basename($_SERVER['PHP_SELF']);
 					<li>
 						<span>No Page Limits</span>
 						<p>
-							The ultimate purpose of your website should be to support your online business goals, regardless of the amount of pages required.
+							The ultimate purpose of your website should be to support your online business goals, regardless of the amount of pages required.*
 						</p>
-						<!-- <p>
-							The most important thing is to have a website that supports your online business. So whether that means a 5-page website or a 50 page website, makes no difference to us.
-						</p>
-						<p>
-							We believe a website is done when it's done and that there isn't a one-size-fits-all website for everyone. When you work with us, we don't hold you to a set number of pages.
-						</p> -->
-						<!-- <small>
-							Read more about <a href="/peak/faq.php#unlimited-pages">Unlimited Pages</a>.
-						</small> -->
 					</li>
 					<li>
 						<span>Mobile Friendly &amp; Responsive</span>
 						<p>
 							No question about it: <a href="#">Mobile Browsing is on the Rise</a>, which means your website has to look good and function everywhere &mdash; but don't sweat it, crafting mobile-friendly websites is just another day at the office for us.
 						</p>
-						<!-- <p>
-							More than ever <a href="linkToCitation" class="inline-link">mobile search is on the rise</a>. This means that people expect your site to work &ndash; and look good &ndash; on everything. When you work with us, you can rest assured that your product will meet your audiences demands.
-						</p> -->
 					</li>
 					<li>
 						<span>SEO Enhanced</span>
 						<p>
 							Websites need traffic in order to thrive. We build our sites on top of industry leading best-practices to ensure your website can be understood and index by all the major search engines.
 						</p>
-						<!-- <p>
-							There's a lot that goes into our websites that enable it to be easily understood and <a href="/peak/faq.php#search-engine-index" class="inline-link">indexed</a> by search engines. Of course, getting to the top of the search results doesn't happen over night; with the right foundation, however, you can make getting started a lot easier.
-						</p>
-						<small>
-							If optimizing the <a href="/peak/faq.php#search-engine-visibility">Search Engine Visibility</a> of your website and maximizing your potential customers is something you're interested in, please see our <a href="/peak/website-services/website-optimiziation-services.php">Website Optimizition Services</a> page and take the first step to being found online.
-						</small> -->
 					</li>
 					<li>
 						<span>Accessible</span>
 						<p>
 							Think of accessible websites like accessibility ramps for brick-and-mortar establishments. Although, often overlooked by other web companies, we believe that your website should be accessible to everyone.
 						</p>
-						<!-- <p>
-							When it comes to your online business, your website should be accessible to everyone. Unfortunately, this isn't always the case. In fact, you may be denying a portion of your customer base without even knowing it. You can think of accessible websites like accessibility ramps for a brick-and-mortar establishments. We do our best to ensure that your website is accessible to the widest range of potential customers.
-						</p> -->
-						<!-- <small>
-							If you'd like to learn more about Web Accessibility Standards, visit the <a href="https://www.w3.org/WAI/">Web Accessibility Initiative</a> website.
-						</small> -->
 					</li>
 					<li>
 						<span>Optimized for Conversion</span>
 						<p>
 							Because at the end of the day your website is a tool for turning visitors into customers, conversion is at the core of every website we build.
 						</p>
-						<!-- <p>
-							At the end of the day, your website should perform a useful function for your business. Converting visitors is at the core of the website development process.
-						</p> -->
-						<!-- <p>
-							In our thinking, the purpose of having a website or online business is to convert customers or to achieve a certain results. Fully understanding the problem is the first step in introducing a solution. When we build a website for your business, we always keep top of mind the goals of your website.
-						</p> -->
 					</li>
 				</ul>
-				<!-- <section id="additional-services">
-					<h3>Additional Services</h3>
-					<p>
-						Need something a little more specialized? Have a look at our additional services.
-					</p>
-					<ul>
-						<li><a href="content-creation-services.php">Content Creation Services</a></li>
-						<li><a href="#">Logo &amp; Graphic Design Services</a></li>
-						<li><a href="personal-and-corporate-branding.php">Personal Branding and Corporate Branding Services</a></li>
-					</ul>
-				</section> -->
+
 			</section>
 			<!-- additional services -->
 
-			<!-- pricing -->
-			<!-- <section>
-				<h3>Pricing</h3>
-				<p>
-					here will we show a pricing table with a few examples. Each example will have a select button which will go to a contact form that will wrap the customer. Make sure to include which selection they chose in the form return.
-				</p>
-			</section> -->
 		</section>
 		<!--/ end of first section -->
 
 	</main>
 
 	<!-- Website Maintenance -->
-	<section class="aside">
+	<!-- <section class="aside">
 		<div class="container">
 			<h2>Need Help Getting your Site Online?</h2>
 			<p>
@@ -133,7 +86,6 @@ $activePage = basename($_SERVER['PHP_SELF']);
 				Learn how we can help get your website online quickly and easily by visition our<br> <a href="/peak/website-services/website-maintenance-services.php">Website Maintenance Services</a> page.
 			</p>
 		</div>
-
-	</section>
+	</section> -->
 
 <?php require_once('../_includes/footer.php') ?>
