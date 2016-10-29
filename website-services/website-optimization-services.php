f1c18efd4854f08a6dbf23d21350fbf81182ac9b
David Gaskin <davidburkegaskin@gmail.com>
2016-10-28 20:09:42 -0700

    update website optimization copy

--- website-services/website-optimization-services.php
+++ website-services/website-optimization-services.php
@@ -8,59 +8,56 @@ $activePage = basename($_SERVER['PHP_SELF']);
 <?php require_once('../_includes/header.php') ?>
 
 	<main class="container service-container">
-
 		<!-- Web Optimization Services -->
 		<section class="intro-service">
 			<div class="">
 				<h2 class="invisible"><?php echo $h2; ?></h2>
-				<span></span>
+				<span>Your website should work <em>for</em> you.</span>
+				<p>
+					Anyone who's serious about the success of their online business knows the importance of optimizing their web presence for site visitors and search engine visibility.
+				</p>
+				<p>Take your online business to the next level. Learn about our <a href="#">Website Optimization Plan</a>.</p>
+			</div>
+			<section class="section">
+				<h3>Website Optimization Plan</h3>
 				<p>
 
 				</p>
-				<p><a href="#"></a></p>
-			</div>
+				<ul class="feature-list">
+					<li>
+						<span>Personal Keyword Strategy</span>
+						<p>
+							Build your content around search terms that drive your core audience and optimize traffic to your site to boost conversion.
+						</p>
+					</li>
+					<li>
+						<span>Powerful Competitor Insights</span>
+						<p>
+							Gain an in-depth view into the marketing strategies of your competitors and realize what unique opportunities exist to catalize the growth of your business.
+						</p>
+					</li>
+					<li>
+						<span>Optimal Search Engine Visibility</span>
+						<p>
+							Turn your website into a peak performer and capitilize on industry leading techniques to optimize your websites search visibility.
+						</p>
+					</li>
+					<!-- <li>Tracking Of Keywords</li> -->
+					<!-- <li>Adword Campaigns</li> -->
+					<!-- <li>Local SEO</li> -->
+					<li>Galvanize your Website Authority
+						<p>
+							Develop your domain authority and set the foundation for consistently strong page rank
+						</p>
+					</li>
+				</ul>
+				<a href="/peak/contact.php">Click here for a Personal Website Assessment</a>
 
-			<!-- Included Services -->
-
+			</section>
 		</section>
-
 	</main>
 
 
-<!-- <h2>Affordable SEO Packages</h2>
 
-<p>
-	Anyone who's serious about the success of their online business understands the importance of optimizing their website for site visitors and the search engines. An optimized website not only improves your visibility on the search engine result pages but improves the user experience for your site visitors, which ultimately leads to higher levels of success.
-</p>
-<p>
-	Anyone who's serious about the success of their online business understands the importance of optimizing their website for site visitors and the search engines. An optimized website not only improves your visibility on the search engine result pages but improves the user experience for your site visitors, one more point here and ultimately, leads to more sales and more business success.
-</p>
-<p>
-	Search Engine Optimization, or SEO as it's commonly referred to in the industry, is the ongoing process of remaining visible in the search engine result pages while simultaneously and consistently connecting to your target audience, that select group of website visitors who drive your business farther and allow you to get closer to reaching your business goals.
-</p>
-<p>
-	Let Peak Creative guide you towards full site optimization. We begin
-</p>
-<p>
-	Have your online business working <em>for you</em>.
-</p>
-
-<h2>What's included in the Website Optimization Service?</h2>
-
-* security (SSL) certificate
-	* firewall, HTTPS
-* SEO Campaign Management
-* Keyword Research & Analysis
-* Unlimited Support
-* Unlimited Content Updates
-Google Analytics
-
-
-Maybe include an SEO strategy development diagram or model of the process.
-1. Understand what the company offers, exactly what are their services? Be as specific as possible.
-2. Thorough Keyword Research + Distribution plan based on website goals and content strategy.
-3. Build Content around Keyword research.
-
-"Content created around thorough keyword research based on your target market. Why is this important?"" -->
 
 <?php require_once('../_includes/footer.php') ?>
