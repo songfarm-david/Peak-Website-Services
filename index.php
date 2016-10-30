2786d1fdcb01e4d663328bfcf498b6b63f8ec894
David Gaskin <davidburkegaskin@gmail.com>
2016-10-30 13:13:45 -0700

    refactor index.php

--- index.php
+++ index.php
@@ -6,78 +6,58 @@ $h2 = "";
 ?>
 <?php require_once('_includes/header.php') ?>
 
-	<section id="landing-page">
-		<h2 class="hide">What We Do</h2>
-		<img class="hide" src="" alt="Focus shot of a large, snow-covered mountain peak" />
-		<div id="landing-page-intro" class="container">
-			<span id="headline">Put Your Business<br> On <span class="accent-color">Top</span></span>
-			<div>
-				<p>
-					Website Services for business owners looking to elevate their online potential.
-				</p>
-				<p>
-					<a href="/peak/website-services/">Learn about our Website Services Plans</a>
-				</p>
+	<main>
+		<section id="landing-page">
+			<h2 class="hide">What We Do</h2>
+			<img class="hide" src="" alt="Focus shot of a large, snow-covered mountain peak" />
+			<div id="landing-page-intro" class="container">
+				<span id="headline">Put Your Business<br> On <span class="accent-color">Top</span></span>
+				<div>
+					<p>
+						Website Services for business owners looking to elevate their online potential.
+					</p>
+					<p>
+						<a href="/peak/website-services/">Learn about our Website Services Plans</a>
+					</p>
+				</div>
 			</div>
-		</div>
-	</section>
-
-	<!-- Core Values -->
-	<section id="about">
-		<h2 class="hide">Why Choose Us</h2>
-		<div class="container">
-			<div id="about-intro">
-				<p>
-					There are a lot of web companies out there, so..<br> <span class="effect">why work with us?</span>
-				</p>
-				<p>
-					<a href="/peak/website-services/">Learn about our Website Services Plans</a>
-				</p>
+		</section>
+		<!-- Core Values -->
+		<section id="about">
+			<h2 class="hide">Why Choose Us</h2>
+			<div class="container">
+				<div id="about-intro">
+					<p>
+						There are a lot of web companies out there, so..<br> <span class="effect">why work with us?</span>
+					</p>
+					<p>
+						<a href="/peak/website-services/">Learn about our Website Services Plans</a>
+					</p>
+				</div>
+				<ul id="key-list">
+					<li>Eye on the Bigger Picture
+						<p>
+							While there are many steps along the way, we always keep top of mind your business goals.
+						</p>
+					</li>
+					<li>Attention to Detail
+						<p>
+							Because it's the little things that make the biggest difference, quality always come before quantity.
+						</p>
+					</li>
+					<li>Personal Service and Attention
+						<p>
+							Choosing a smaller client-base means we're always just a phone call away. You can think of us as your personal IT guy.
+						</p>
+					</li>
+					<li>Translate your Vision into Beautiful Reality
+						<p>
+							We balance creativity with industry best practices to deliver high-quality products that reflect the uniqueness of your business.
+						</p>
+					</li>
+				</ul>
 			</div>
-			<ul id="key-list">
-				<li>Eye on the Bigger Picture
-					<p>
-						While there are many steps along the way, we always keep top of mind your business goals.
-					</p>
-				</li>
-				<li>Attention to Detail
-					<p>
-						Because it's the little things that make the biggest difference, quality always come before quantity.
-					</p>
-				</li>
-				<li>Personal Service and Attention
-					<p>
-						Choosing a smaller client-base means we're always just a phone call away. You can think of us as your personal IT guy.
-					</p>
-				</li>
-				<li>Translate your Vision into Beautiful Reality
-					<p>
-						We balance creativity with industry best practices to deliver high-quality products that reflect the uniqueness of your business.
-					</p>
-				</li>
-
-
-				<!-- <li>Guaranteed Satisfaction
-					<p>
-						We go the extra mile because your satisfaction is our number one priority.
-					</p>
-				</li> -->
-			</ul>
-		</div>
-
-
-
-		<!-- <aside id="guarantee">
-			<h3>100% Satisfaction Guarantee</h3>
-			<p>
-				If, for any reason, you are not satisfied with our work, we will give you your money back.
-			</p>
-		</aside>
-		<blockquote cite="http://">
-			Think of us as your personal IT team.
-		</blockquote> -->
-
-
-	</section>
+		</section>
+	</main>
 
 	<?php require_once('_includes/footer.php') ?>
