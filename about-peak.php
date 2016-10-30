fe9c8b99206d9100ccfa9f35de50273c854079f0
David Gaskin <davidburkegaskin@gmail.com>
2016-10-30 13:13:13 -0700

    basic layout styles: about peak

--- about-peak.php
+++ about-peak.php
@@ -5,35 +5,20 @@ $activePage = basename($_SERVER['PHP_SELF']);
 ?>
 <?php require_once('_includes/header.php') ?>
 
-<header>
-	<h1 class="h3 hide">About Peak Website Services</h1>
-	<?php include_once('_includes/navigation.php') ?>
-</header>
-
 	<main>
-
-		<div id="about-1">
+		<section id="about-peak">
 			<div class="container">
-				<span>
-					<span class="spin">Leave the</span> technical <span class="spin">work</span> of building and maintaining a website <span class="spin">to us</span>.
-				</span>
+				<p class="headline">
+					<span class="hero">Leave the</span> technical <span class="hero">work</span> of building and maintaining a website <span class="hero">to us</span>.
+				</p>
 				<p>
 					Peak Website Services is a <u>complete solution</u> to business owners who are not tech-savvy and would rather run their business than worry about code.
 				</p>
-				<a href="/free-quote">
-					Get a Free Quote
-				</a>
-				<a href="/pricing/">
-					View Pricing
-				</a>
+				<p>
+					Our professional developers and designers combine website and brand creation, website development and maintenance, and strategy-driven website optimization into affordable website services designed to maximize your online success and make handling the technical aspects of having an online business simple.
+				</p>
 			</div>
-		</div>
-		<div id="about-2">
-			<p>
-				Our professional developers and designers combine website and brand creation, website development and maintenance, and strategy-driven website optimization into affordable website services designed to maximize your online success and make handling the technical aspects of having an online business simple.
-			</p>
-		</div>
-
+		</section>
 	</main>
 
 
