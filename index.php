380c44646031e8f723c7f75470c85502dc75a5f4
David Gaskin <davidburkegaskin@gmail.com>
2016-10-29 23:17:58 -0700

    re-style about section index.php

--- index.php
+++ index.php
@@ -26,45 +26,43 @@ $h2 = "";
 	<section id="about">
 		<h2 class="hide">Why Choose Us</h2>
 		<div class="container">
-			<div class="row">
-				<div class="col col-6">
-					<p id="about-intro">
-						There are a lot of web companies out there, so..<br> <span class="h2">why work with us?</span>
-					</p>
-					<a href="/peak/about-peak.php" class="cta-link">Read more about Peak</a>
-				</div>
-				<div class="col col-6">
-					<ul>
-						<li>Eye on the Bigger Picture
-							<p>
-								While there are many steps along the way, we always keep top of mind your business goals.
-							</p>
-						</li>
-						<li>Attention to Detail
-							<p>
-								Because it's the little things that make the biggest difference, quality always come before quantity.
-							</p>
-						</li>
-						<li>Personal Service and Attention
-							<p>
-								Choosing a smaller client-base means we're always just a phone call away. You can think of us as your personal IT guy.
-							</p>
-						</li>
-						<li>Translate your Vision into Beautiful Reality
-							<p>
-								We balance creativity with industry best practices to deliver high-quality products that reflect the uniqueness of your business.
-							</p>
-						</li>
-
-
-						<!-- <li>Guaranteed Satisfaction
-							<p>
-								We go the extra mile because your satisfaction is our number one priority.
-							</p>
-						</li> -->
-					</ul>
-				</div>
+			<div id="about-intro">
+				<p>
+					There are a lot of web companies out there, so..<br> <span class="effect">why work with us?</span>
+				</p>
+				<p>
+					<a href="/peak/about-peak.php">Read more about Peak</a>
+				</p>
 			</div>
+			<ul id="key-list">
+				<li>Eye on the Bigger Picture
+					<p>
+						While there are many steps along the way, we always keep top of mind your business goals.
+					</p>
+				</li>
+				<li>Attention to Detail
+					<p>
+						Because it's the little things that make the biggest difference, quality always come before quantity.
+					</p>
+				</li>
+				<li>Personal Service and Attention
+					<p>
+						Choosing a smaller client-base means we're always just a phone call away. You can think of us as your personal IT guy.
+					</p>
+				</li>
+				<li>Translate your Vision into Beautiful Reality
+					<p>
+						We balance creativity with industry best practices to deliver high-quality products that reflect the uniqueness of your business.
+					</p>
+				</li>
+
+
+				<!-- <li>Guaranteed Satisfaction
+					<p>
+						We go the extra mile because your satisfaction is our number one priority.
+					</p>
+				</li> -->
+			</ul>
 		</div>
 
 
