ec03d1199364278642c5909cb733bb2a2ebf6a06
David Gaskin <davidburkegaskin@gmail.com>
2016-11-09 14:50:59 -0800

    replace cta with cta banner include

--- index.php
+++ index.php
@@ -65,13 +65,7 @@ $canonical = "http://peakwebsites.ca/";
 			</div>
 		</section>
 
-		<a href="/contact.php" style="text-decoration:none;">
-			<section id="final-cta">
-				<div class="">
-					<h2>Receive a FREE Estimate <i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></h2>
-				</div>
-			</section>
-		</a>
+		<?php include('../_includes/theme/banner.php'); ?>
 
 
 	</main>
