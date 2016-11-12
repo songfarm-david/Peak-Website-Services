bbfa18d532dd0b19eca496518eefb9d98b96da70
David Gaskin <davidburkegaskin@gmail.com>
2016-11-11 21:13:48 -0800

    write bylines for website services

--- website-services/index.php
+++ website-services/index.php
@@ -10,11 +10,22 @@ $activePage = basename($_SERVER['PHP_SELF']);
 	<!-- Website Services -->
 	<section id="website-services">
 		<h2 class="hide"><?php echo $h2; ?></h2>
+		<div class="container">
+			<p class="h2">
+				Choose from our Website Services:
+			</p>
+		</div>
 		<!-- services -->
 		<div id="service-container">
 			<!-- website creation services -->
 			<div>
 				<h3>Website<br> <span class="sea-green">Development</span> <span class="at-only">Services</span></h3>
+				<!-- <p class="service-byline">
+					Develop what you already have or create something new that's all your own.
+				</p> -->
+				<p>
+					Add on to your existing or create something that's all your own.
+				</p>
 				<ul>
 					<li>Website Creation</li>
 					<li>Website Redesign</li>
@@ -28,6 +39,15 @@ $activePage = basename($_SERVER['PHP_SELF']);
 			<!-- website maintenance services -->
 			<div>
 				<h3>Website<br> <span class="cedar-chest">Maintenance</span> <span class="at-only">Services</span></h3>
+				<!-- <p class="service-byline">
+					It's like having your own personal IT guy so you can run your business.
+				</p> -->
+				<!-- <p class="service-byline">
+					For having your own personal IT guy to handle the <em>technical stuff</em>.
+				</p> -->
+				<p>
+					Let us handle the technical stuff while you handle your business.
+				</p>
 				<ul>
 					<li>Website Hosting</li>
 					<li>Domain Management</li>
@@ -41,6 +61,9 @@ $activePage = basename($_SERVER['PHP_SELF']);
 			<!-- website optimization services -->
 			<div>
 				<h3>Website<br> <span class="electric-blue">Optimization</span> <span class="at-only">Services</span></h3>
+				<p>
+					Get noticed in the search results and put your website over the edge.
+				</p>
 				<ul>
 					<li>Search Visibility</li>
 					<li>Keyword Strategy</li>
