25465bc52700f2564bdac1716e34cc8f37554a4f
David Gaskin <davidburkegaskin@gmail.com>
2016-11-13 00:01:48 -0800

    clean up markup; remove older copy; add hr

--- website-services/index.php
+++ website-services/index.php
@@ -20,29 +20,20 @@ $activePage = basename($_SERVER['PHP_SELF']);
 			<!-- website creation services -->
 			<div>
 				<h3>Website <span class="highlight">Development</span> Services</h3>
-				<!-- <p class="service-byline">
-					Add on to your existing or create something that's all your own.
-				</p> -->
-				<!-- <span class="divider"><img src="/peak/_images/ornate-separator.svg" alt="" /></span> -->
-				<!-- <a href="website-development-services.php">Select</a> -->
+				<hr>
+				<a href="website-development-services.php">Choose</a>
 			</div>
 			<!-- website maintenance services -->
 			<div>
 				<h3>Website <span class="highlight">Maintenance</span> Services</h3>
-				<!-- <p class="service-byline">
-					Let us handle the technical stuff while you handle your business.
-				</p> -->
-				<!-- <span class="divider"><img src="/peak/_images/ornate-separator.svg" alt="" /></span> -->
-				<!-- <a href="website-maintenance-services.php">Select</a> -->
+				<hr>
+				<a href="website-maintenance-services.php">Choose</a>
 			</div>
 			<!-- website optimization services -->
 			<div>
 				<h3>Website <span class="highlight">Optimization</span> Services</h3>
-				<!-- <p class="service-byline">
-					Optimize your online presence for optimal results.
-				</p> -->
-				<!-- <span class="divider"><img src="/peak/_images/ornate-separator.svg" alt="" /></span> -->
-				<!-- <a href="website-optimization-services.php">Select</a> -->
+				<hr>
+				<a href="website-optimization-services.php">Choose</a>
 			</div>
 		</div><!--/ end services -->
 
