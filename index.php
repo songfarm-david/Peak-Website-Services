3d4f5a0f9ba8cffb908f548557ce61b8ee8c7546
David Gaskin <davidburkegaskin@gmail.com>
2016-11-13 17:37:42 -0800

    commit main index as part of 7ba8070 include header change

--- index.php
+++ index.php
@@ -1,44 +1,6 @@
 	<?php require_once('_includes/header.php') ?>
 
-	<!-- preloader gif -->
-	<div id="preloader"></div>
-
-	<!-- help ticket -->
-	<article id="ticket-newsletter">
-		<span id="ticket" class="fa fa-question-circle-o" aria-hidden="true"></span>
-		<h3>Register for our weekly reports</h3>
-		<p>
-			Register to receive our weekly expert financial reports for cutting-edge financial advise and data-driven projections.<br>
-		</p>
-		<form class="" action="index.html" method="post">
-			<input type="text" name="name" value="" placeholder="Name">
-			<input type="email" name="email" value="" placeholder="Email">
-			<input type="submit" name="name" value="Send">
-		</form>
-	</article>
-
 	<div id="main-container">
-
-		<!-- main site header -->
-		<h1 class="hide">Hollis Wealth</h1>
-
-		<header id="site-header">
-			<div class="row">
-				<div class="col col-7">
-					<!-- Logo Here -->
-					<div id="logo">
-						<img class="logo" src="_assets/temp-logo.png" alt="" height="90" />
-					</div>
-				</div>
-				<div class="col col-5">
-					<!-- CTA -->
-					<div id="call-now" class="float-right">
-						<a href="#" class="phone"><span class="fa fa-phone" aria-hidden="true"></span>&nbsp;(012)-345-6789</a>
-					</div>
-					<?php include_once('_includes/navigation.php') ?>
-				</div>
-			</div>
-		</header>
 
 		<!-- Highlights Carousel -->
 		<section id="highlights">
