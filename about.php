468498a65107cce1ffaefe3ffacd30a44be49206
David Gaskin <davidburkegaskin@gmail.com>
2016-11-13 16:20:01 -0800

    create about page

--- about.php
+++ about.php
@@ -1,23 +1,9 @@
 <?php require_once('_includes/header.php') ?>
 
-		<main>
+	<main class="container">
+		<h3>About Page</h3>
+	
+	</main>
 
-			<span>
-				Leave the technical work of building and maintaining a website to us.
-			</span>
-			<p>
-				Peak Website Services is a complete solution to business owners who are not tech-savvy and would rather run their business than worry about writing code.
-			</p>
-			<p>
-				Our professional developers and designers combine website and brand creation, website development and maintenance, and strategy-driven website optimization into affordable website services designed to maximize your online success and make handling the technical aspects of having an online business a breeze.
-			</p>
-			<p>
-				Read more about our Website Services
-			</p>
-			<p>
-				View Pricing
-			</p>
 
-		</main>
-
-		<?php require_once('_includes/footer.php') ?>
+<?php require_once('_includes/footer.php') ?>
