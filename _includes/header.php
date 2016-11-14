7ba8070549993af437348a0be8d6ca5a28a31788
David Gaskin <davidburkegaskin@gmail.com>
2016-11-13 16:40:33 -0800

    move site header and navigation to header include

--- _includes/header.php
+++ _includes/header.php
@@ -20,3 +20,41 @@
 				<script src="_js/vendor/html5shiv.js"></script>
 				<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 		<![endif]-->
+
+		<!-- main site header -->
+		<h1 class="hide">Hollis Wealth</h1>
+
+		<header id="site-header" class="container">
+			<div class="row">
+				<div class="col col-7">
+					<!-- Logo Here -->
+					<div id="logo">
+						<img class="logo" src="_assets/temp-logo.png" alt="" height="90" />
+					</div>
+				</div>
+				<div class="col col-5">
+					<!-- CTA -->
+					<div id="call-now" class="float-right">
+						<a href="#" class="phone"><span class="fa fa-phone" aria-hidden="true"></span>&nbsp;(012)-345-6789</a>
+					</div>
+					<?php include_once('_includes/navigation.php') ?>
+				</div>
+			</div>
+		</header>
+
+		<!-- preloader gif -->
+		<!-- <div id="preloader"></div> -->
+
+		<!-- help ticket -->
+		<article id="ticket-newsletter">
+			<span id="ticket" class="fa fa-question-circle-o" aria-hidden="true"></span>
+			<h3>Register for our weekly reports</h3>
+			<p>
+				Register to receive our weekly expert financial reports for cutting-edge financial advise and data-driven projections.<br>
+			</p>
+			<form class="" action="index.html" method="post">
+				<input type="text" name="name" value="" placeholder="Name">
+				<input type="email" name="email" value="" placeholder="Email">
+				<input type="submit" name="name" value="Send">
+			</form>
+		</article>
