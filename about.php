84ced68c5dbaaa03d531ad478d27a9594f8dd0bd
David Gaskin <davidburkegaskin@gmail.com>
2016-11-13 16:29:15 -0800

    add markup about page

--- about.php
+++ about.php
@@ -1,8 +1,37 @@
 <?php require_once('_includes/header.php') ?>
 
 	<main class="container">
-		<h3>About Page</h3>
-	
+		<h3>About West Coast Wealth Management</h3>
+		<article id="dave-bio" class="bio">
+			<h3 class="at-only">About Dave Charlebios</h3>
+			<p>
+				Bacon ipsum dolor amet pariatur mollit quis picanha velit. Dolore ut ham porchetta strip steak enim shankle venison corned beef et. Non prosciutto qui pancetta, tempor turkey bresaola flank aliqua ham venison. Swine pastrami velit brisket, culpa incididunt beef porchetta adipisicing ipsum prosciutto chicken flank eiusmod. Strip steak consequat sausage officia spare ribs ball tip. In sunt ad andouille irure.
+			</p>
+			<p>
+				Nostrud sed beef est. T-bone ad turkey shank. Porchetta leberkas kevin turducken ut aute, capicola officia nostrud short ribs venison. Quis aliquip id swine, shoulder porchetta pork do tri-tip. Bacon duis proident, hamburger ut eiusmod brisket voluptate tongue capicola t-bone shank commodo.
+			</p>
+			<figure>
+				<img src="" alt="Headshot of " />
+				<figcaption>
+					<small>A special note about you.</small>
+				</figcaption>
+			</figure>
+		</article>
+		<article id="steve-bio" class="bio">
+			<h3 class="at-only">About Stephen Gaskin</h3>
+			<p>
+				Spicy jalapeno bacon ipsum dolor amet ipsum landjaeger ut, shoulder biltong doner exercitation turducken swine drumstick elit capicola strip steak in dolor. Strip steak laboris landjaeger pork chop, chicken kielbasa meatball. Pork burgdoggen laborum duis. Officia pancetta tri-tip, turkey veniam irure pariatur ea tempor ground round t-bone non enim corned beef beef.
+			</p>
+			<p>
+				Tail t-bone laboris pork shankle bacon. Dolor capicola pork chop salami biltong commodo. Laborum tri-tip minim fugiat. Swine pancetta chicken pork aute veniam quis sunt ut sed short loin andouille pork belly sint burgdoggen.
+			</p>
+			<figure>
+				<img src="" alt="Headshot of " />
+				<figcaption>
+					<small>A special note about you.</small>
+				</figcaption>
+			</figure>
+		</article>
 	</main>
 
 
