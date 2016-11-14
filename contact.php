5047e0500b1a71a21adde85203de3943b118bfa1
David Gaskin <davidburkegaskin@gmail.com>
2016-11-13 19:35:17 -0800

    create contact page add basic form; create contact less page

---
+++ contact.php
@@ -0,0 +1,18 @@
+<?php require_once('_includes/header.php') ?>
+
+	<main id="about-page" class="container">
+		<div class="mini-hero">
+			<h2>Contact West Coast Wealth Management</h2>
+		</div>
+		<section>
+			<h3>Contact Form</h3>
+			<form class="" action="" method="post">
+
+			</form>
+		</section>
+
+		<?php require_once('_includes/footer.php') ?>
+
+	</main>
+
+<?php require_once('_includes/foot.php') ?>
