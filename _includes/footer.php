94b7f66c7c15e7bb0f81b49f4cf8a1d1b5d7655a
David Gaskin <davidburkegaskin@gmail.com>
2016-11-13 19:27:38 -0800

    move footer files to footer include

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,1 +1,64 @@
+<!-- Footer -->
+<footer>
+	<!-- Address -->
+	<div class="row">
+		<div class="col col-4">
+			<section id="address">
+				<h4>Address</h4>
+				<div id="address-container">
+					<span class="address">P.O Box V5N 1P9</span><br>
+					<span class="address">123 Granville Street, Suite 700</span><br>
+					<span class="address">Victoria, BC V7Y 1G5</span><br>
+					<!-- phone contact -->
+					<a href="#" class="phone"><span class="fa fa-phone" aria-hidden="true"></span>&nbsp;(012)-345-6789</a>
 
+					<!-- <p>
+
+						Contact Us: <a href="tel:1234567890" class="contact contact-phone">123-456-7890</a>
+					</p> -->
+				</div>
+			</section>
+		</div>
+		<div class="col col-8">
+			<!-- mission statement -->
+			<section id="mission-statement">
+				<h4>Mission Statement</h4>
+				<p>
+					Hollis Wealth is a tart icing fruitcake gummies marshmallow dessert soufflé chupa chups. Oat cake cookie pudding. Fruitcake marshmallow chupa chups donut sweet sugar plum muffin jujubes cheesecake. Cake chupa chups I love toffee powder jelly marshmallow. Jujubes I love marshmallow tootsie roll marshmallow I love donut. Chocolate cake cookie cake cupcake gingerbread.
+				</p>
+			</section>
+		</div>
+	</div>
+	<!-- Site Map -->
+	<div class="row">
+		<div class="col col-12">
+			<section id="site-map">
+				<h3 class="h4">Site Map</h3>
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">About</a></li>
+					<li><a href="#">Services</a></li>
+					<li><a href="#">Insights</a></li>
+					<li><a href="#">Contact</a></li>
+				</ul>
+			</section>
+		</div>
+	</div>
+	<!-- Disclaimer -->
+	<div class="row">
+		<div class="col col-12">
+			<section id="disclaimer">
+				<h5>Disclaimer</h5>
+				<small>
+					Jelly beans ice cream powder carrot cake topping I love danish. Chocolate I love liquorice candy sweet roll sesame snaps. Wafer lollipop jelly beans donut I love chocolate cake cake marzipan. Biscuit danish pudding apple pie halvah danish marshmallow apple pie bonbon. Bonbon biscuit lollipop tart sesame snaps lollipop tart jelly beans. Gingerbread jelly fruitcake powder wafer dessert I love. Lemon drops liquorice jelly. Tart cake bear claw. Sweet roll oat cake sweet I love dragée I love sugar plum I love pudding. Lemon drops wafer pastry gummies ice cream wafer soufflé oat cake marzipan.
+				</small>
+			</section>
+		</div>
+	</div>
+	<!-- Copyright -->
+	<section id="copyright">
+		<h5 class="hide">Copyright</h5>
+		<small>&copy; <?php echo date("Y"); ?> Hollis Wealth</small>
+	</section>
+
+</footer>
