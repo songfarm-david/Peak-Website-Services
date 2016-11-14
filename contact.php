d7072af422476389267d3aa4978cce752a6825b4
David Gaskin <davidburkegaskin@gmail.com>
2016-11-13 20:10:57 -0800

    add form + basic styles

--- contact.php
+++ contact.php
@@ -1,14 +1,31 @@
 <?php require_once('_includes/header.php') ?>
 
-	<main id="about-page" class="container">
+	<main id="contact-form" class="container">
 		<div class="mini-hero">
 			<h2>Contact West Coast Wealth Management</h2>
 		</div>
 		<section>
-			<h3>Contact Form</h3>
+			<h3 class="">Contact Us</h3>
 			<form class="" action="" method="post">
-
+				<label for="name">Name <span>(required)</span>
+					<input type="text" name="name" value="" placeholder="Name">
+				</label>
+				<label for="phone">Phone <span>(required)</span>
+					<input type="phone" name="name" value="" placeholder="Phone">
+				</label>
+				<label for="email">Email
+					<input type="phone" name="name" value="" placeholder="Phone">
+				</label>
+				<label>Please get in touch:
+					<textarea name="name" rows="8" cols="40"></textarea>
+				</label>
 			</form>
+			<article id="call-us">
+				<h3>Call Us</h3>
+				<p>
+					<span>Call us at: <a href="tel:2501534567">(250) 123-4567</a></span>
+				</p>
+			</article>
 		</section>
 
 		<?php require_once('_includes/footer.php') ?>
