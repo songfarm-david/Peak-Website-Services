1f8f5fd7d50508b45a7ce7ebff0c3cc4cde2b287
David Gaskin <davidburkegaskin@gmail.com>
2016-11-21 17:16:28 -0800

    update markup website services; add animations; style animations for mobile

--- website-services/index.php
+++ website-services/index.php
@@ -19,46 +19,31 @@ $activePage = basename($_SERVER['PHP_SELF']);
 		<div id="service-container">
 
 			<!-- website creation services -->
-			<a href="website-development-services.php">
-				<div>
-					<h3>Website Development Services</h3>
-					<!-- <p class="service-byline">
-						Add on to your existing or create something that's all your own.
-					</p> -->
-					<!-- <span class="divider"><img src="/peak/_images/ornate-separator.svg" alt="" /></span> -->
-					<!-- <a href="website-development-services.php">Select</a> -->
-				</div>
-			</a>
+			<div class="grow-left">
+				<h3>Website Development Services</h3>
+				<a href="website-development-services.php"><span class="at-only">Select</span></a>
+			</div>
 
 			<!-- website maintenance services -->
-			<a href="website-maintenance-services.php">
-				<div>
-					<h3>Website Maintenance Services</h3>
-					<!-- <p class="service-byline">
-						Let us handle the technical stuff while you handle your business.
-					</p> -->
-					<!-- <span class="divider"><img src="/peak/_images/ornate-separator.svg" alt="" /></span> -->
-					<!-- <a href="website-maintenance-services.php">Select</a> -->
-				</div>
-			</a>
+			<div class="grow-center">
+				<h3>Website Maintenance Services</h3>
+				<a href="website-maintenance-services.php"><span class="at-only">Select</span></a>
+			</div>
 
 			<!-- website optimization services -->
-			<a href="website-optimization-services.php">
-				<div>
-					<h3>Website Optimization Services</h3>
-					<!-- <p class="service-byline">
-						Optimize your online presence for optimal results.
-					</p> -->
-					<!-- <span class="divider"><img src="/peak/_images/ornate-separator.svg" alt="" /></span> -->
-					<!-- <a href="website-optimization-services.php">Select</a> -->
-				</div>
-			</a>
+			<div class="grow-right">
+				<h3>Website Optimization Services</h3>
+				<a href="website-optimization-services.php"><span class="at-only">Select</span></a>
+			</div>
 
 		</div><!--/ end services -->
 
-		<section>
+		<!-- <section>
 			<h3>See our Frequently Asked Questions</h3>
-		</section>
+			<p>
+
+			</p>
+		</section> -->
 	</section>
 
 <?php require_once('../_includes/footer.php') ?>
