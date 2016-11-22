524573556e6a452fb9a6b1806563dcb6071756ad
David Gaskin <davidburkegaskin@gmail.com>
2016-11-21 19:26:03 -0800

    finish first styles website services

--- website-services/index.php
+++ website-services/index.php
@@ -19,19 +19,19 @@ $activePage = basename($_SERVER['PHP_SELF']);
 		<div id="service-container">
 
 			<!-- website creation services -->
-			<div class="grow-left">
+			<div class="grow grow-left">
 				<h3>Website <span>Development</span> Services</h3>
 				<a href="website-development-services.php"><span class="at-only">Select</span></a>
 			</div>
 
 			<!-- website maintenance services -->
-			<div class="grow-center">
+			<div class="grow grow-center">
 				<h3>Website <span>Maintenance</span> Services</h3>
 				<a href="website-maintenance-services.php"><span class="at-only">Select</span></a>
 			</div>
 
 			<!-- website optimization services -->
-			<div class="grow-right">
+			<div class="grow grow-right">
 				<h3>Website <span>Optimization</span> Services</h3>
 				<a href="website-optimization-services.php"><span class="at-only">Select</span></a>
 			</div>
@@ -46,6 +46,19 @@ $activePage = basename($_SERVER['PHP_SELF']);
 		</section> -->
 		<i class="fa fa-angle-down" aria-hidden="true"></i>
 
+	</section>
+
+	<section id="service-cta">
+		<div class="">
+			<h2>Need More?&nbsp;<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></h2>
+			<p>
+				<a href="/peak/contact.php">Receive a FREE estimate</a>
+				<br>
+				&nbsp;or
+				<br>
+				<a href="#">See our FAQ</a>
+			</p>
+		</div>
 	</section>
 
 <?php require_once('../_includes/footer.php') ?>
