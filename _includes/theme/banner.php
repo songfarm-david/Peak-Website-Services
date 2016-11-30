0a1ef7f153fde7812c3eff4f04aa99995a258a8f
David Gaskin <davidburkegaskin@gmail.com>
2016-11-29 21:09:47 -0800

    add secondary text to cta-banner

--- _includes/theme/banner.php
+++ _includes/theme/banner.php
@@ -1,7 +1,8 @@
 <a href="/peak/contact.php" style="text-decoration:none;">
 	<section id="final-cta">
 		<div class="">
-			<h2>Receive a FREE Estimate&nbsp;<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></h2>
+			<h2>FREE Estimate&nbsp;<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></h2>
+			<p>Tell us about your online business goals and we'll get back with a no-strings-attached estimate.</p>
 		</div>
 	</section>
 </a>
