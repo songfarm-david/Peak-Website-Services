1af97bbb4e5622907698ee2288b6050a0bfb187c
David Gaskin <davidburkegaskin@gmail.com>
2016-12-03 10:48:11 -0800

    remove quicklink from footer; adjust height offset in global less

--- _includes/footer.php
+++ _includes/footer.php
@@ -1,11 +1,4 @@
 	<footer>
-		<!-- <article id="quick-quote">
-			<h3 class="at-only">Contact Us</h3>
-			<span class="h2">Need some help?</span>
-			<p>
-				<a href="/peak/contact.php" style="text-transform:capitalize;">Get in Touch</a>.
-			</p>
-		</article> -->
 		<nav id="site-map">
 			<h4 class="h3">Site Map</h4>
 			<ul>
