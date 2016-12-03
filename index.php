b9b07020fb185c967d0bcf31e70484218bd5a7c8
David Gaskin <davidburkegaskin@gmail.com>
2016-12-02 21:42:01 -0800

    add new landing page text copy

--- index.php
+++ index.php
@@ -10,100 +10,53 @@ $canonical = "http://peakwebsites.ca/";
 	<main>
 
 		<!-- Landing Page -->
-		<section id="landing-page">
-			<h2 class="hide">Welcome to Peak Website Services</h2>
+		<section id="landing-page"><!-- id="business-top" -->
+
 			<img class="hide" src="_images/air-triangulated.jpg" alt="The peak of a large, snow-covered mountain peak" />
-			<div id="landing-page-intro" class="container fadeInUp">
-				<span id="headline">Put Your Business On&nbsp;<span>Top</span></span>
-				<div>
-					<p>
-						Elevate your business with affordable, personalized <em>Website&nbsp;Services</em> and take the headache out of having a website.
-					</p>
-					<p>
-						<a href="/contact.php">Find out how we can help!</a>
-					</p>
-				</div>
+
+			<div class="container fadeInUp" id="landing-page-intro">
+				<h1 id="headline">Put Your Business On&nbsp;<span>Top</span></h1>
+				<p>
+					Take your online business to the next level with our suite of <em>Website Services</em>.
+				</p>
 			</div>
+
 			<i class="fa fa-angle-down" aria-hidden="true"></i>
+
 		</section><!--/ end of Landing Page -->
 
+
 		<!-- What We Do -->
-		<section id="what-we-do">
-			<div class="container">
-				<h3 class="hide">What we do</h3>
-				<h3>With you every step of the way</h3>
+		<section id="elevate-business">
 
-				<!-- row 1 -->
-				<div class="row">
-					<div class="col col-4">
-						<p>Get a state-of-the-art, industry standard website suited to your unique needs and vision.</p>
-					</div>
-					<div class="col col-4">
-						<p>We'll set it up, host it for you, and keep you up to speed with regular checkups so you remain in the drivers' seat.</p>
-					</div>
-					<div class="col col-4">
-						<p>More than that, we'll guide you along the complicated world of Search Engine Optimization so that you reach the top of the mountain.</p>
-					</div>
-				</div>
+			<h2>Elevate Your Business Potential</h2>
+			<p>
+				Get a state-of-the-art website suited to your unique needs and vision. We'll set it up, host it for you, and keep you in the loop with monthly Website Visibility Reports. More than that, we'll guide you along the complicated world of Search Engine Optimization and help your business reach the top of the search results.
+			</p>
 
-				<p>
-					We're <a href="#">Peak Website Services</a> &mdash; your best choice for reaching the top.
-				</p>
-				<p>
-					<a href="#">Read more about our Website Services</a>
-				</p>
-
-			</div><!--/ end of container -->
 		</section><!--/ what we do -->
 
+
 		<!-- Why A Website -->
-		<section id="why">
-			<h2 class="hide">What We Do</h2>
-			<div class="container">
-				<div id="about-intro">
-					<p>
-						A website is your business card online, so...
-						<span class="effect">why do you need a website?</span>
-					</p>
-				</div>
-				<!-- benefits key list -->
-				<ul id="key-list">
-					<li>More Credibility
-						<p>
-							A strong website communicates to everyone that you mean business. It immediately establishes credibility in the eyes of your customers and makes them much more likely to engage with your business.
-						</p>
-					</li>
-					<li>More Exposure
-						<p>
-							9 out of 10 consumers rely on the internet to locate and evaluate goods and services. A properly positioned website exposes your business to those most likely to buy from you.
-						</p>
-					</li>
-					<!-- <li>More Freedom
-						<p>
-							When you
-						</p>
-					</li> -->
-					<li>More Money
-						<p>
-							Online Business Success isn&apos;t for the faint of heart. With strong guidance, the right plan, and a little bit of determination, you will attract the right kind of customers and increase your revenue.
-						</p>
-					</li>
-				</ul><!--/ benefits key list -->
-
-				<!-- <p id="about-cta">
-					<a href="/peak/about.php">Read More About Us</a>
-				</p> -->
-
-			</div>
+		<section id="website-important">
+			<h2>Why Is A Website So Important for Small-to-Medium Sized Businesses?</h2>
+			<dl class="">
+				<dt>More Credibility</dt>
+				<dd>A strong website communicates to everyone that you mean business. It immediately establishes credibility in the eyes of your customers and makes them much more likely to engage with your business.</dd>
+				<dt>More Exposure</dt>
+				<dd>A well positioned website attracts the right kind of customer to your business. It puts you in front of your core audience and acts as a continuous stream of highly targeted, relevant traffic to your website.</dd>
+				<dt>More Money</dt>
+				<dd>9 out of 10 consumers today rely on the internet to locate and evaluate goods and services. The right website expands market reach, builds consumer relationships, and grows revenue.</dd>
+			</dl>
 		</section>
 
-		<section>
+		<!-- <section>
 			<p>Everything you need, all in one place</p>
 			<p>Learn how we can help</p>
-		</section>
+		</section> -->
 
 		<?php // include('_includes/theme/banner.php'); ?>
 
 	</main>
 
-	<?php require_once('_includes/footer.php') ?>
+<?php require_once('_includes/footer.php') ?>
