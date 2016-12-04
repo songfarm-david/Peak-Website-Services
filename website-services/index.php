b7ab395a44e11f9f23df9aea5786cb37e1e60f8c
David Gaskin <davidburkegaskin@gmail.com>
2016-12-03 18:26:51 -0800

    add website service table content

--- website-services/index.php
+++ website-services/index.php
@@ -2,63 +2,133 @@
 $title = "Website Services | Peak Website Services";
 $description = "Peak Website Services include: website development services, website maintenance services, website optimization services, and more";
 $h1 = "Peak Website Services";
-$h2 = "Website Services";
+$h2 = "";
 $activePage = basename($_SERVER['PHP_SELF']);
 ?>
 <?php require_once('../_includes/header.php') ?>
 
 	<!-- Website Services -->
 	<section id="website-services">
-		<h2 class="hide"><?php echo $h2; ?></h2>
+
 		<div class="container">
-			<p class="h2">
-				Select a service
-			</p>
+			<h2>With You Every Step of the Way</h2>
+			<p>We have a Website Service for every stage of your online journey &mdash; <br>Plus, the expertise to make sure you reach the summit.</p>
 		</div>
-		<!-- services -->
-		<div id="service-container">
 
-			<!-- website creation services -->
-			<div class="grow grow-left">
-				<h3>Website <span>Development</span> Services</h3>
-				<a href="website-development-services.php"><span class="at-only">Select</span></a>
-			</div>
+		<!-- services table -->
+		<article id="service-table">
 
-			<!-- website maintenance services -->
-			<div class="grow grow-center">
-				<h3>Website <span>Maintenance</span> Services</h3>
-				<a href="website-maintenance-services.php"><span class="at-only">Select</span></a>
-			</div>
+			<h3 class="at-only">Website Services Table</h3>
 
-			<!-- website optimization services -->
-			<div class="grow grow-right">
-				<h3>Website <span>Optimization</span> Services</h3>
-				<a href="website-optimization-services.php"><span class="at-only">Select</span></a>
-			</div>
+			<!-- NOTE: table will exist as back-up. A flexbox version will be implemented afterwards. -->
+			<table role="table" cellspacing="0">
+				<caption class="at-only">Website Services Table</caption>
+				<col>
+				<col>
+				<col>
+				<thead>
+					<tr>
+						<th role="columnheader" scope="col" aria-owns="body-dev-1">Website Development</th>
+						<th role="columnheader" scope="col" aria-owns="body-maint-1">Website Maintenance</th>
+						<th role="columnheader" scope="col" aria-owns="body-optim-1">Website Optimization</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td id="body-dev-1">The <a href="/peak/website-services/website-development-services.php">website development service</a> is the perfect opportunity to start off on the right foot. </td>
+						<td id="body-maint-1">The <a href="/peak/website-services/website-maintenance-services.php">website maintenance service</a> allows you, the small-to-medium size business owner, the freedom to focus on what you do best. Not the code behind your website.</td>
+						<td id="body-optim-1">The <a href="/peak/website-services/website-optimization-services.php">website optimization service</a> is perfect for business owners who want to take their business to the top.</td>
+					</tr>
+					<tr>
+						<td>We strive, first, to understand your particular needs, then deliver to you an industry up-to-date, mobile-responsive, accessible, and beautifully designed website that is unique to your brand and vision.</td>
+						<td>Your website is hosted on a quick and secure Canadian webserver and a portion of hours are dedicated each month to ensure that while the internet changes, your website stays healthy and protected.</td>
+						<td>We dedicate a portion of hours each month to your business, analyzing keywords and optimizing your online presence.</td>
+					</tr>
+					<tr>
+						<td>Maybe there needs to be some text inside of here</td>
+						<td>Some of what's included:</td>
+						<td>Some of what's included:</td>
+					</tr>
+					<tr>
+						<td class="stop-row"></td>
+						<td>Maybe there needs to be some text inside of here</td>
+						<td>Maybe there needs to be some text inside of here</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>Maybe there needs to be some text inside of here</td>
+						<td>Maybe there needs to be some text inside of here</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>Maybe there needs to be some text inside of here</td>
+						<td>Maybe there needs to be some text inside of here</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td>Maybe there needs to be some text inside of here</td>
+						<td>Maybe there needs to be some text inside of here</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td class="stop-row"></td>
+						<td>Maybe there needs to be some text inside of here</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td>Maybe there needs to be some text inside of here</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td>Maybe there needs to be some text inside of here</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td>Maybe there needs to be some text inside of here</td>
+					</tr>
+					<tr>
+						<td></td>
+						<td></td>
+						<td>Maybe there needs to be some text inside of here</td>
+					</tr>
+				</tbody>
+				<tfoot>
+					<!-- if applicable -->
+				</tfoot>
+			</table>
 
-		</div><!--/ end services -->
-
-		<!-- <section>
-			<h3>See our Frequently Asked Questions</h3>
-			<p>
-
-			</p>
-		</section> -->
-		<i class="fa fa-angle-down" aria-hidden="true"></i>
+		</article>
 
 	</section>
 
-	<section id="service-cta">
-		<div class="">
-			<h2>Need More?&nbsp;<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i></h2>
-			<p>
-				<a href="/peak/contact.php">Receive a FREE estimate</a>
-				<br>
-				&nbsp;or
-				<br>
-				<a href="#">See our FAQ</a>
-			</p>
-		</div>
-	</section>
+
+	<!-- services -->
+	<!-- <div id="service-container"> -->
+
+		<!-- website creation services -->
+		<!-- <div class="grow grow-left">
+			<h3>Website <span>Development</span> Services</h3>
+			<a href="website-development-services.php"><span class="at-only">Select</span></a>
+		</div> -->
+
+		<!-- website maintenance services -->
+		<!-- <div class="grow grow-center">
+			<h3>Website <span>Maintenance</span> Services</h3>
+			<a href="website-maintenance-services.php"><span class="at-only">Select</span></a>
+		</div> -->
+
+		<!-- website optimization services -->
+		<!-- <div class="grow grow-right">
+			<h3>Website <span>Optimization</span> Services</h3>
+			<a href="website-optimization-services.php"><span class="at-only">Select</span></a>
+		</div> -->
+
+	<!-- </div> -->
+	<!--/ end services -->
+
+	<!-- <i class="fa fa-angle-down" aria-hidden="true"></i> -->
 
 <?php require_once('../_includes/footer.php') ?>
