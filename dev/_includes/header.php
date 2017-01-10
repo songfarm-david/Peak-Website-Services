210167f7270296d025ed7318fc46efef86ab86c4
David Gaskin <davidburkegaskin@gmail.com>
2017-01-09 22:30:23 -0800

    clean up header
    removed unnecessary x-ua-compat ie edge as researched and found unnecessary

--- dev/_includes/header.php
+++ dev/_includes/header.php
@@ -2,12 +2,10 @@
 <html lang="en-us" class="no-js">
 	<head>
 		<meta charset="utf-8">
-		<meta http-equiv="x-ua-compatible" content="ie=edge">
 		<title><?php echo $title; ?></title>
 		<meta name="description" content="<?php echo $description; ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<?php echo $canon = (!empty($canonical)) ? "<link rel=\"canonical\" href=\"" . $canonical . "\">\n" : ""; ?>
-		<!-- Detects and modifies html class 'no-js' if JavaScript is enabled -->
 		<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)</script>
 
 		<!-- conditional & critical CSS here -->
@@ -16,8 +14,8 @@
 		<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,700,900" rel="stylesheet">
 	</head>
 	<body>
-		<!--[if lt IE 8]>
-      <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+		<!--[if lte IE 9]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
     <![endif]-->
 
 		<header>
