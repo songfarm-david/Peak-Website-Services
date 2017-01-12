d3844cffd471459fc21bd686d82c5c53a213cf34
David Gaskin <davidburkegaskin@gmail.com>
2017-01-12 13:29:28 -0800

    add flexbox fallback for 3 column section using modernizr

--- dev/index.php
+++ dev/index.php
@@ -41,11 +41,6 @@ $canonical = "http://peakwebsites.ca/";
 				 <p>We specialize in providing <a href="#">peak&nbsp;website&nbsp;services</a> to busy business owners who don't want to worry about the code.</p>
 				 <p class="highlight">So, why work with us?</p>
 
-
-			<!-- </div> -->
-
-
-
 			<!-- link to the contact form -->
 
 		</section>
@@ -54,18 +49,21 @@ $canonical = "http://peakwebsites.ca/";
 
 			<div id="columns" class="">
 				<div>
-					<h3>Commitment to Quality</h3><p>We don't settle for "just okay". We go above and beyond the call to provide you with service you can rely on. We call ourselves Peak Websites for a reason!</p>
+					<h3>Commitment to Quality</h3>
+					<p>We don't settle for "just okay". We go above and beyond the call to provide you with service you can rely on. We call ourselves Peak Websites for a reason!</p>
 				</div>
 				<div>
-					<h3>Industry Expertise</h3><p>Websites are what we do and part of that is keeping up with the ever-changing world of the web. You can sleep comfortably knowing, that with us, you're in good hands.</p>
+					<h3>Industry Expertise</h3>
+					<p>Websites are what we do and part of that is keeping up with the ever-changing world of the web. You can sleep comfortably knowing, that with us, you're in good hands.</p>
 				</div>
 				<div>
-					<h3>Accessible Web</h3><p>We believe in trust and transparency in business and all that we do. Built  and we want to work with like-minded people. We're helping to make the web a better place.</p>
+					<h3>Accessible Web</h3>
+					<p>We believe in trust and transparency in business and all that we do. Built  and we want to work with like-minded people. We're helping to make the web a better place.</p>
 
 				<!-- <p>We  we're a limited-size company to the amount of clients we can take on, those we do work with will have a partner in business.</p> -->
 				</div>
 			</div>
-			
+
 			<a href="contact.php" class="callout-link" title="Contact Us">Elevate Your Online Business</a>
 
 
