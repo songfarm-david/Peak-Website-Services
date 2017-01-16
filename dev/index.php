3166dc0062f2578a34102af93737830b26089289
David Gaskin <davidburkegaskin@gmail.com>
2017-01-16 13:58:03 -0800

    update copy in landing page; change over canonical value
    switched to https canonical in index page and contact form

--- dev/index.php
+++ dev/index.php
@@ -3,7 +3,7 @@ $title = "Peak Website Services";
 $description = "Peak Website Services offers website development services, website maintenance services, and website optimization services for peak online business owners.";
 $h1 = $title;
 $h2 = "";
-$canonical = "http://peakwebsites.ca/";
+$canonical = "https://peakwebsites.ca/";
 ?>
 <?php require_once('_includes/header.php') ?>
 
@@ -13,8 +13,8 @@ $canonical = "http://peakwebsites.ca/";
 			<img class="at-only" src="_images/air-triangulated.jpg" alt="The peak of a large, snow-covered mountain on a crisp and cloudy day" />
 			<div class="container callout fadeInUp">
 				<h1 class="">Put Your Business On&nbsp;<span>Top</span></h1>
-				<p>Get everything you need to compete in today's online world, all in one place.</p>
-				<a href="#complicated-web" class="callout-link" title="today's world">Learn How</a>
+				<p>Get everything you need to compete in today's online world.</p>
+				<a href="#complicated-web" id="page-1-cta" class="callout-link" title="Learn How">Learn How</a>
 			</div>
 		</section><!--/ end of Landing Page -->
 
@@ -22,9 +22,9 @@ $canonical = "http://peakwebsites.ca/";
 		<section id="complicated-web">
 			<h2>Today's Web Is <em>Hard</em></h2>
 			 <p>In today's online world, it's important that you make a strong first impression, but having a good website today isn't what it used to be &mdash; making sure it works across devices, is quick and easy to use, and makes a great first impression on potential customers can be a full time job!</p>
-			 <p id="special"><b>That's where we come in.</b></p>
-			 <p>We specialize in providing <a href="#">peak&nbsp;website&nbsp;services</a> to busy business owners who don't want to worry about the code.</p>
-			 <p class="highlight">So, why work with us?</p>
+			 <p id="special"><b>That's where <em>I</em> come in.</b></p>
+			 <p>I specialize in providing <a href="#">peak&nbsp;website&nbsp;services</a> to busy business owners who don't want to worry about the code.</p>
+			 <p class="highlight">So, why work with me?</p>
 		</section><!--/ end Page two -->
 
 		<!-- Floating Section -->
@@ -32,15 +32,15 @@ $canonical = "http://peakwebsites.ca/";
 			<div id="columns" class="">
 				<div>
 					<h3>Commitment to Quality</h3>
-					<p>We don't settle for <em>"just okay"</em>. We go above and beyond the call to provide you with the kind of service you can feel good about. <em>We call ourselves Peak Websites for a reason!</em></p>
+					<p>I don't settle for <em>"just okay"</em>. I go above and beyond the call to provide you with the kind of service you can feel good about. <em>I chose the name Peak Websites for a reason!</em></p>
 				</div>
 				<div>
 					<h3>Industry Expertise</h3>
-					<p>Websites are what we do and part of that is keeping up with the ever-changing world of the web. When you work with us, you can relax and focus on the more important things.</p>
+					<p>Websites are what I do and part of that is keeping up with the ever-changing world of the web. When you work with me, you can relax and focus on the more important things.</p>
 				</div>
 				<div>
 					<h3>Satisfaction Guaranteed</h3>
-					<p>We want to make the web a better place, and that starts with being the kind of business you feel good working with. We stand behind everything we do. Guaranteed.</p>
+					<p>I want to make the web a better place, and that starts with being the kind of business you feel good working with. I stand behind everything I do. Guaranteed.</p>
 				</div>
 			</div>
 			<a href="contact.php" title="contact form">Elevate Your Online Business Today</a>
