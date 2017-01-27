159a029e3e4b2d8d730250184d109080ae4b8f7f
David Gaskin <davidburkegaskin@gmail.com>
2017-01-27 14:07:00 -0800

    complete basic portfolio styles

--- dev/portfolio/index.php
+++ dev/portfolio/index.php
@@ -11,33 +11,36 @@ $activePage = basename($_SERVER['PHP_SELF']);
 
 		<!-- Portfolio -->
 		<section id="portfolio">
-			<h1 class="hide"><?php echo $h1; ?></h1>
+			<h1><?php echo $h1; ?></h1>
 
-			<!-- Recent Work -->
-			<section>
-				<h2><?php echo $h2; ?></h2>
+
+				<p>Below are some of the more recent projects I have undertaken. Click on an image to view the live website.</p>
 
 				<div class="row">
 					<div class="col col-6">
-						<div id="cirs" class="image-port"></div>
+						<a href="https://peakwebsites.ca/cirs/" target="cirs_iframe"><div id="cirs" class="image-port"></div></a>
+						<iframe src="#" id="cirs_iframe"></iframe>
 					</div>
 					<div class="col col-6">
-						<div id="songfarm" class="image-port"></div>
+						<a href="http://songfarm.ca" target="songfarm_iframe"><div id="songfarm" class="image-port"></div></a>
+						<iframe src="#" id="songfarm_iframe"></iframe>
 					</div>
 				</div>
 
 				<div class="row">
 					<div class="col col-6">
-						<div id="luna" class="image-port"></div>
+						<a href="http://www.canoamassage.com/" target="luna_iframe"><div id="luna" class="image-port"></div></a>
+						<iframe src="#" id="luna_iframe"></iframe>
 					</div>
 					<div class="col col-6">
-						<div id="hollis" class="image-port"></div>
+						<a href="https://peakwebsites.ca/hollis-wealth/" target="hollis_iframe"><div id="hollis" class="image-port"></div></a>
+						<iframe src="#" id="hollis_iframe"></iframe>
+
 					</div>
 				</div>
 
 
 
-			</section><!--/ end Recent Work -->
 
 		</section><!--/ end Portfolio -->
 
