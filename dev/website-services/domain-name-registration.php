dc15edb5068d16393da0698bf9e97a0f7baecced
David Gaskin <davidburkegaskin@gmail.com>
2017-02-05 14:07:17 -0800

    update domain name registration copy

--- dev/website-services/domain-name-registration.php
+++ dev/website-services/domain-name-registration.php
@@ -8,38 +8,55 @@ $h2 = "";
 <?php require_once('../_includes/header.php') ?>
 
 	<main>
-		<h2>Domain Name Registration</h2>
+
 		<p class="h2">Begin the journey to online success with the right domain name.</p>
+		<p>A domain name is the cornerstone of your digital presence, so it's a good idea to take the time to educate yourself a little on <a href="#what-is-a-domain-name">what a domain name is</a>, <a href="#why-domain-important">why it's important</a>, and learn a <a href="#domain-name-tips">few tips</a> on choosing a good one.</p>
+		<p>If, by the end of this article, you still have questions or need help choosing or registering a domain name, feel free to <a href="#">contact us</a>. We would be happy to help you in selecting a great domain name for your online business and we can <a href="#register-a-domain">register your domain name</a>, too.</p>
 
-		<h2>What is a Domain Name?</h2>
-		<p>A domain name is your unique address on the web. When somebody refers you to their website, they will tell you the domain name of their website for you to visit. For example, the domain name of <em>this</em> website is <code>peakwebsites.ca</code> (sometimes you will see a &rdquo;www&ldquo; before the domain name. That is not part of a domain name, don't let that confuse you). Other examples of domain names are <code>google.ca</code> or <code>msn.com</code>. Anyone can register a domain name and usually, it is one of the first things you do when you decide you want to have a website.</p>
+		<h2 id="what-is-a-domain-name">What is a Domain Name?</h2>
+			<p>A domain name is your unique address on the web. When somebody refers you to their website, they will tell you the domain name of their website for you to visit. For example, the domain name of <em>this</em> website is <code>peakwebsites.ca</code> (sometimes you will see a &rdquo;www&ldquo; before the domain name. That is not part of a domain name, don't let that confuse you). Other examples of domain names are <code>google.ca</code> or <code>msn.com</code>. Anyone can register a domain name and usually, it is one of the first things you do when you decide you want to have a website.</p>
 
-		<h2>Why is Having a Domain Name Important?</h2>
-		<p>Having a domain name is important for a number of reasons. One of the biggest reasons is <em>credibility</em>. A business without a credible website is less trustworthy in the eyes of consumers, and a credible website starts with a good <strong>domain name</strong>. Another reason why having a domain name is important has to do with your <em>brand</em>. Oftentimes, a domain name becomes synonymous with the company it represents. Think <code>apple.com</code> or <code>amazon.com</code>. On their own, they don't really have much meaning, but because we are familiar with these brands, they become symbols for the business. And to think, it started with a domain name...</p>
-		<p>Lastly, a domain name is important because &ndash; and let's face it &ndash; the internet is here to stay. Due to the sheer amount of people who use the internet, having a domain name and website creates fantastic opportunities to reach new customers and achieve greater business success.</p>
-		<p class="cta-link">Would you like to optimize your website for success? Learn more about <a href="/website-services/website-optimization-services.php">Website Optimization</a>.</p>
+			<h3>What is the <code>.com</code> at the end?</h3>
+			<p>A website domain name always ends in what is called a Top-Level Domain or TLD, for short. We will talk more about those later.</p>
 
-		<h2>Tips on Choosing A Domain Name</h2>
-		<p>Before you decide on a domain name, take time to consider the following:</p>
-		<ul>
+		<h2 id="why-domain-important">Why is Having a Domain Name Important?</h2>
+			<p>Having a domain name is important for a number of reasons. One of the biggest reasons is <em>credibility</em>. A business without a website (or a poor website) risks losing trust in the eyes of consumers, as they are less likely to engage with your business; a credible website starts with a <strong>good domain name</strong>.</p>
+			<p>Another reason why having a domain name is important has to do with your company image or <em>brand</em>. Oftentimes, a domain name becomes synonymous with the company it represents. Think <code>apple.com</code> or <code>amazon.com</code>. On their own, they don't really have much meaning, but because we are familiar with these brands, they become symbols for the business. And to think, it all started with a domain name...</p>
+			<p>Lastly, a domain name is important because &ndash; and let's face it &ndash; the internet is here to stay. Due to the sheer amount of people who use the internet, having a domain name and website creates fantastic opportunities to reach new customers and achieve greater business success.</p>
+			<!-- <p class="cta-link">Would you like to optimize your website for success? Learn more about <a href="/website-services/website-optimization-services.php">Website Optimization</a>.</p> -->
+
+		<h2 id="domain-name-tips">Tips on Choosing A Domain Name</h2>
+			<p>Before you decide on a domain name, here a few tips to consider:</p>
+			<ul>
 			<li><b>Memorable</b> &mdash; A strong domain name is one that is easy to remember and easy to say. If you can convey your business in a catchy, memorable way through a domain name, it'll go a long way in helping others reach your website as well as make it easier for you, the site owner, to refer people to your site. Don't choose a name that's overly long, complicated, or hard to remember!</li>
 			<li><b>Concise</b> &mdash; It is often in your best interests to choose a domain name that quickly tells the consumer what your website is all about, for an obvious (if rather bias) example, look at the name <code>peakwebsites.ca</code>. It is pretty clear right away what peakwebsites is all about, is it not? Websites! You don't need to re-invent the wheel. You want concise and memorable. If you are a local company, you may consider using local identifiers (such as the name of the city you operate in) in your domain name to help distinguish your unique domain and offer more information about the products/services you provide.</li>
 			<li><b>Relevant</b> &mdash; Before selecting a domain name, it's important that the name you choose conveys the right message to your audience. You will also want to make sure that the domain name reflects your businesses brand. It is a fine line between choosing a name that is relevant in the minds of your consumers but also speaks to your companies values and branding message. Remember that, when in doubt, keep it simple: if you are in the business of making pizzas, then by all means, you'd be wise to include the word &rdquo;pizza&ldquo; in your domain name!</li>
-			<li><b>Top-Level Domain</b> &mdash; The <em><code>.com</code></em> or <em><code>.org</code></em> part of a domain name is called the Top-Level Domain (or TLD, for short). Recently, there has been an explosion of new TLD's to choose from, for example, <code>.band</code>, <code>.photography</code>, <code>.cars</code>. Sometimes, when the TLD you want is either taken, or too expensive to register, you can find a great, creative TLD that fits well with your company brand. It's equally important to note that certain TLDs are <em>country-specific</em> (for example, <code>.ca</code> is the TLD for Canada) or related to non-profits (<code>.org</code>) or government organizations (<code>.gov</code>). <a href="https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains">See a full list of Top-Level Domains</a>.</li>
+			<li><b>Top-Level Domain</b> &mdash; The <em><code>.com</code></em> or <em><code>.org</code></em> part of a domain name is called the Top-Level Domain (or TLD, for short). Recently, there has been an explosion of new TLD's to choose from, for example, <code>.band</code>, <code>.photography</code>, <code>.cars</code>. Sometimes, when the TLD you want is either taken, or too expensive to register, you can find a great, creative TLD that fits well with your company brand. It's equally important to note that certain TLDs are <em>country-specific</em> (for example, <code>.ca</code> is the TLD for Canada) or related to non-profits (<code>.org</code>) or government organizations (<code>.gov</code>). <a href="https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains" rel="nofollow">See a full list of Top-Level Domains</a>.</li>
 		</ul>
 
 		<h2>How Do I Register a Domain Name?</h2>
-		<p>The first step to registering a domain (once you have settled on the domain name you want) is to find a Registrar. There are a lot of Registrars out there but be choosy in who you go with; there are some Registrars who will do some <a href="https://www.easydns.com/10-things-to-know-before-you-register/">nasty stuff</a> in the name of profit, so be sure to find an ethical Registrar you trust. Once you find a Registrar, he or she will do an availablity search for the domain name you want. If it's available and priced to sell, then your Registrar will register the domain name on your behalf. You will then be the owner of that domain name - Yippee! The period for which you can register a domain name is variable: you may register a domain name for 1, 2, up to 10 years (sometimes even 100 years!). Whatever length you choose is up to you. If you only registered a domain name for 1 year, you have the first option to re-register that domain name before it expires, so there is little need to worry that you will lose the domain name you worked so hard to come up with! Sometimes it saves money to register your domain for a longer period and sometimes not. A good registrar will provide you with all your options as well as help you to make the best choice for you based on your particular situation.</p>
+			<p>The first step to registering a domain name (once you have settled on the domain name you want) is to find a Registrar. There are a lot of Registrars out there but be choosy in who you go with; there are some Registrars who will do some <a href="https://www.easydns.com/10-things-to-know-before-you-register/">nasty stuff</a> in the name of higher profits, so be sure to find an ethical Registrar you trust.</p>
+			<p>Once you find a Registrar, he or she will do an availablity search for the domain name you want. If it's available and priced to sell, then your Registrar will register the domain name on your behalf. You will then be the owner of that domain name - Yippee!</p>
+			<p>The period for which you can register a domain name is variable: you may register a domain name for 1, 2, up to 10 years (sometimes even 100 years!). Whatever length you choose is up to you. If you only registered a domain name for 1 year, you have the first option to re-register that domain name before it expires, so there is little need to worry that you will lose the domain name you worked so hard to come up with! Sometimes it saves money to register your domain for a longer period and sometimes not. A good registrar will provide you with all your options as well as help you to make the best choice for you based on your particular needs. <a href="#register-a-domain">Ready to register a domain name?</a></p>
 
-		<h2>Register A Domain Name</h2>
-		<p>Peak Websites offers reliable and affordable Domain Name Registration. Allow us to register your domain name on your behalf and to assist you in finding a great domain name! We conduct our business in an ethical and fully transparent way, whereby at the end of the process, you will own your own domain name and have complete control over it. Additionally, should you have any questions or concerns along the way, we are always available to provide assistance to you at no extra cost.</p>
-		<!-- <p>Peak Websites exists to provide clear, honest, simple, and reliable website services to those with little to no technical experience. No technical mumbo-jumbo, no hidden costs, just valuable services provided to you at an affordable price.</p> -->
+		<h2 id="register-a-domain">Register A Domain Name</h2>
+		<p>Have you decided on your domain name? Great! <a href="/contact.php">Contact Us</a> to register your domain name. By the end of the process you will own your very own domain name!</p>
 
-		<p><a href="tel:17785879220">Call</a> or <a href="/contact.php">Contact Us Today</a> if you would like to register or need help with a domain name.</p>
+			<h3>Worried About Price?</h3>
+			<p>Most domain names typically cost between $10-$30 to register for one year although some domain names can sell for <a href="https://www.google.ca/search?q=most+expensive+domain+names&oq=most+expensive+domain+names&aqs=chrome..69i57.5532j0j4&sourceid=chrome&ie=UTF-8" rel="nofollow">millions of dollar</a>. Sometimes, it may be more cost effective to register your domain for a longer term, such as 5 or 10 years. Your Registrar should help you to understand what options you have at your disposal and to help you to make an educated decision.</p>
+
+		<h2>I Can't Find a Good Domain Name?</h2>
+		<p>We'll help you out. <a href="/contact.php">Contact us today</a> and we'll help you in finding a great domain name for your business. We can even <a href="/contact.php">Register Your Domain Name</a> for you.</p>
 
 		<h2>I've Got a Domain Name, Now What?</h2>
-		<p>Once you have successfully registered a domain name, you now own it, but isn't doing much good unless it connects to a website or email account.</p>
-		<p>Don't have a website or not happy with the one you have? Visit our <a href="/website-services/website-development-services.php">Website Development Page</a> to learn about your options and how we can help you to have a great website!</p>
+		<p>Once you have successfully registered a domain name, you now own it, but it isn't doing you much good unless you have a website to go along with it.</p>
+		<p><a href="/index.php">Peak Website Services</a> can build you a great website. Have a look at our <a href="/portfolio/">Portfolio</a> to see some past work, or learn about our <a href="">web design</a> and <a href="">custom website development</a> services.</p>
+
+		<h2>Contact Us</h2>
+		<a href="">PHONE</a>
+		<a href="">Contact Form</a>
+
+
 	</main>
 
 
