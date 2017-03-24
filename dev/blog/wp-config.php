1e4632e8f2a2baf675827c3d16ad6e6f89d87a01
David Gaskin <davidburkegaskin@gmail.com>
2017-03-24 11:12:18 -0700

    update wp-config for database connectivity

--- dev/blog/wp-config.php
+++ dev/blog/wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'peakwebs_pkblog');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'peakwebs_pkblog');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', '5S(3p6bH7@');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
