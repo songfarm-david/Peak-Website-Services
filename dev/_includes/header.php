bcd192530fc14b415a55d04c0816372297812d4a
David Gaskin <davidburkegaskin@gmail.com>
2017-03-24 11:09:29 -0700

    refactor header.php

--- dev/_includes/header.php
+++ dev/_includes/header.php
@@ -10,24 +10,24 @@
 
 		<!-- WebFontLoader: https://www.sitepoint.com/improve-page-performance-font-loader/ -->
 		<script type="text/javascript">
-		var WebFontConfig = {
-			google: {
-				families: [ 'Raleway:300,400,700,800,900' ]
-			},
-			timeout: 4000
-		};
-		(function(){
-			var wf = document.createElement("script");
-			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-				'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-			wf.async = 'true';
-			document.head.appendChild(wf);
-		})();
+			var WebFontConfig = {
+				google: {
+					families: [ 'Raleway:300,400,700,800,900' ]
+				},
+				timeout: 4000
+			};
+			(function(){
+				var wf = document.createElement("script");
+				wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+					'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+				wf.async = 'true';
+				document.head.appendChild(wf);
+			})();
 		</script>
 
 		<!-- conditional & critical CSS here -->
 		<?php
-		$css;
+			$css;
 
 			// IF IS INDEX PAGE
 			if ($_SERVER['PHP_SELF'] == '/peak/dev/index.php') {
