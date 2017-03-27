8ee51a41167b10b50821b92b2814af4a63051079
David Gaskin <davidburkegaskin@gmail.com>
2017-03-26 22:31:09 -0700

    refactor website service services pages

--- dev/website-services/website-maintenance-services.php
+++ dev/website-services/website-maintenance-services.php
@@ -10,64 +10,31 @@ $activePage = basename($_SERVER['PHP_SELF']);
 	<main>
 
 		<!-- service -->
-		<section class="bg-image" id="website-maintenance-service">
+		<section class="bg-image" id="maintenance">
 			<h2 class="hide"><?php echo $h2; ?></h2>
 			<div class="service">
 				<p class="service-title">Take the load off having an online presence</p>
-				<p>From <a href="#">hosting your website</a> and registering a domain name to updating content and fixing bugs, working with Peak is like having your own IT guy on speed-dial. We provide you with friendly, personal support for all things online.</p>
+				<p>From hosting your website and registering a domain name to updating content and fixing bugs, working with Peak is like having your own IT guy on speed-dial. We provide you with friendly, personal support for all things online.</p>
 				<p>Want to change out that landing page image? Need something new on your contact page? <a href="/contact.php">We're there for you</a>.</p>
 				<p>Having a website shouldn't add to your work load. We offer everything you need, all in once place and take the work out of having an online presence.</p>
 				<!-- <p>We monitor your website on our fast, reliable web servers and each quarter present you with <a href="#">Website Growth Report</a> with opportunities for your website and to keep you in the loop with how your website is doing. Should you ever require a change to your website, simply <a href="">call us</a> and we'll take of your concern. <em>You can think of it like having your own IT guy.</em></p> -->
 			</div>
+		</section><!-- #maintenance -->
 
-			<section class="service-types">
-				<h3>Website Maintenance Services Include:</h3>
-				<ul>
-					<li>Website Growth Reports</li>
-					<li>Website Monitoring</li>
-					<li>Safe &amp; Secure Web Hosting</li>
-					<li>Unlimited Content Updates</li>
-					<li>Unlimited Technical Support</li>
-					<li><em>Your own personal IT guy</em></li>
-				</ul>
-				<!-- <p><a href="#">Register today</a> for this limited service.</p> -->
-			</section>
+		<section class="service-types">
+			<h3>Website Maintenance Services Include:</h3>
+			<ul>
+				<li>Website Growth Reports</li>
+				<li>Website Monitoring</li>
+				<li>Safe &amp; Secure Web Hosting</li>
+				<li>Unlimited Content Updates</li>
+				<li>Unlimited Technical Support</li>
+				<li><em>Your own personal IT guy</em></li>
+			</ul>
+			<!-- <p><a href="#">Register today</a> for this limited service.</p> -->
+		</section>
 
 		<p>Let us help you take hold of your website. <a href="/contact.php">Contact Us Today</a>.</p>
-			<!-- <div class="service-intro container">
-				<span>Take the load off having an online presence.</span>
-				<p>
-					Your website is hosted on a quick and secure <em>Canadian webserver</em> and a portion of hours are dedicated each month to ensure that while the internet changes, your website stays <em>healthy</em> and <em>protected</em>.
-				</p>
-			</div> -->
-
-			<article class="article">
-				<h3>Website Monitoring</h3>
-				<p class="article-sub-title">Keeping You Protected so your website can perform</p>
-				<p>Monitoring your website regularly for threats and security standard service procedure.</p>
-			</article>
-
-			<article class="article">
-				<h3>Safe &amp; Secure Web Hosting</h3>
-				<p class="article-sub-title">Keeping You Reliably Online All Year Long</p>
-				<p><a href="">Hosting your website on Canadian soil</a> protects your privacy, but hosting it on our state-of-the-art servers ensures your web pages are served reliably and instantly to your customers.</p>
-				<p class="call-out"><a href="/website-services/website-hosting-services.php">learn more about our Hosting Services</a>.</p>
-			</article>
-
-			<!-- style-NOTE: this section is styled a little differently -->
-			<section class="article">
-				<h3>Unlimited Content Updates, Unlimited Technical Support</h3>
-				<p class="article-sub-title">Working with Peak Website Services is like having your own personal IT guy</p>
-				<p>We carry out updates and are your ongoing technical support when you have questions about your online success.</p>
-				<p>Best of all, you have a single point-of-contact for all your online needs. This greatly simplifies the complexities of having an online business, and allows you to focus on running your business.</p>
-			</section>
-
-			<p>Let us help you take hold of your online success. <br><a href="/contact.php">Contact Us Today</a>.</p>
-
-		</section><!--/ end of website-maintenance-service section -->
-
-		<!-- cta -->
-		<?php //include('../_includes/theme/banner.php'); ?><!--/ end of cta -->
 
 	</main>
 
--- dev/website-services/website-optimization-services.php
+++ dev/website-services/website-optimization-services.php
@@ -22,85 +22,16 @@ $activePage = basename($_SERVER['PHP_SELF']);
 		<section class="service-types">
 			<h3>Website Optimization Services Include:</h3>
 			<ul>
-				<li>On-Page Optimization</li>
+				<li>Web Strategy</li>
+				<li>Technical Optimization</li>
 				<li>Competitor Insights &amp; Analysis</li>
 				<li>Website Analytics</li>
 				<li>Targeted Keyword Research</li>
-				<li>Website Visibility Report</li>
-
+				<li>Personalized Strategy Reports</li>
 			</ul>
-			<!-- <p>Please <a href="#">Register today</a> for this limited service. <em>Only 10 spots available.</em></p> -->
 		</section>
 
-		<p>Let us help you optimize your website. <a href="/contact.php">Contact Us Today</a>.</p>
-
-			<!-- <div class="service-intro">
-				<span>Your website should work <em>for</em> you.</span>
-				<p>
-					We are a strong proponent of the <em>'work smarter not harder'</em> philosophy and make it easy for you to make smarter decisions by knowing what's important to your customers and what's going on with your website.
-				</p>
-			</div> -->
-
-			<!-- service features -->
-			<!-- <section class="service-features" id="website-optimization-plan"> -->
-				<!-- <h3 class="at-only">Website Optimization Plan</h3> -->
-				<!-- features list -->
-				<!-- <ul class="feature-list">
-					<li>
-						<div class="feature-container">
-							<img src="../../_assets/svg-icons/optimize-icons/keyword-strategy.svg" alt="" />
-							<span>Keyword Strategy Personalized to Your Business</span>
-						</div>
-						<p>
-							The only way to succeed on the search pages is to leverage your unique angle and learn how that is relevant to the people you want to target.
-						</p>
-					</li>
-					<li>
-						<div class="feature-container">
-							<img src="../../_assets/svg-icons/optimize-icons/competitor-insights.svg" alt="" />
-							<span>Learn About your Competitors</span>
-						</div>
-						<p>
-							Knowing where your competitor is weak and where they are strong helps you capitilize on your own strengths and minimize areas of lesser need.
-						</p>
-					</li>
-					<li>
-						<div class="feature-container">
-							<img src="../../_assets/svg-icons/optimize-icons/search-visibility.svg" alt="" />
-							<span>Optimized Search Visibility</span>
-						</div>
-						<p>
-							Let us turn your website into a peak performer so that you are always where you need to be online.
-						</p>
-					</li>
-					<li>
-						<div class="feature-container">
-							<img src="../../_assets/svg-icons/optimize-icons/megaphone.svg" alt="" />
-							<span>Learn From the Data</span>
-						</div>
-						<p>
-							We provide you with invaluable web analytics about your market, your competitors, and your key opportunities for growth in the technology world.
-						</p>
-					</li>
-					<li>
-						<div class="feature-container">
-							<img src="../../_assets/svg-icons/optimize-icons/market.svg" alt="" />
-							<span>Flexible to Change</span>
-						</div>
-						<p>
-							We are with you for the long haul, hands on the wheel, and voice in your ear, guiding your decisions to elevate your business online.
-						</p>
-					</li>
-					<li>
-						&nbsp;
-					</li>
-				</ul> -->
-			<!-- </section> -->
-			<!--/ end of service features -->
-
-
-		<!-- cta -->
-		<?php //include('../_includes/theme/banner.php'); ?><!--/ end of cta -->
+		<p>Trust Peak Websites to help you reach your online goals. <a href="/contact.php">Contact Us Today</a>.</p>
 
 	</main>
 
