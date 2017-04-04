53c6188c620c0b29baf7764c20c66f7f1ffdbd3e
David Gaskin <davidburkegaskin@gmail.com>
2017-04-04 13:55:12 -0700

    move google analytics script to include file

---
+++ dev/_includes/gA.php
@@ -0,0 +1,9 @@
+<!-- Google Analytics ID tracking since 10/23/2016. -->
+<script>
+	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+	function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+	e.src='https://www.google-analytics.com/analytics.js';
+	r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+	ga('create','UA-86141289-1','auto');ga('send','pageview');
+</script>
