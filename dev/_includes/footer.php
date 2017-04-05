dab58acfa791b1d8f1c8caf053aa1fcaeae7f682
David Gaskin <davidburkegaskin@gmail.com>
2017-04-04 18:49:20 -0700

    add sitemap to footer, add basic styles

--- dev/_includes/footer.php
+++ dev/_includes/footer.php
@@ -1,14 +1,37 @@
 		<footer>
-			<!-- <section id="social-links">
+			<article id="sitemap" class="sitemap">
+				<ul>
+					<li class="sitemap-title">Company</li>
+					<!-- <li><a href="" title="About Peak Websites">About</a></li> -->
+					<li><a href="/portfolio/" title="Peak Websites Portfolio">Portfolio</a></li>
+					<li><a href="/blog/" title="Peak Website's Blog">Blog</a></li>
+				</ul>
+				<ul>
+					<li class="sitemap-title">Services</li>
+					<li><a href="/website-services/website-development-services.php">Website Development</a></li>
+					<li><a href="/website-services/website-optimization-services.php">Search&nbsp;Engine&nbsp;Optimization</a></li>
+					<li><a href="/website-services/website-hosting-services.php">Website Hosting</a></li>
+					<li><a href="/website-services/website-maintenance-services.php">Website Maintenance</a></li>
+				</ul>
+				<ul>
+					<li class="sitemap-title">Contact</li>
+					<li><a href="/contact.php" title="Request a Free Assessment">Free Assessment</a></li>
+					<li><a href="mailto:david@peakwebsites.ca?subject=Well,%20Hello!" title="Send us an email">Email Us</a></li>
+				</ul>
+				<br>
+				<hr style="clear:both;">
+			</article>
+			<section id="social-links">
 				<h4 class="at-only">Social Links</h4>
 				<ul>
-					<li><a href="https://twitter.com/peak_websites"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-					<li><a href="https://github.com/songfarm-david/"><i class="fa fa-github" aria-hidden="true"></i></a></li>
-					<li><a href="http://stackexchange.com/users/3476745/peak"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a></li>
-					<li><a href="https://www.facebook.com/peakwebsiteservices/" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+					<li><a href="https://www.facebook.com/peakwebsiteservices/" target="_blank" title="Facebook: Peak Websites"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+					<li><a href="https://twitter.com/peak_websites" title="Twitter: Peak Websites"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+					<!-- <li><a href="https://github.com/songfarm-david/"><i class="fa fa-github" aria-hidden="true"></i></a></li> -->
+					<!-- <li><a href="http://stackexchange.com/users/3476745/peak"><i class="fa fa-stack-overflow" aria-hidden="true"></i></a></li> -->
+					<li><a href="https://www.linkedin.com/in/david-gaskin-75339b134/" target="_blank" title="LinkedIn: Peak Websites"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
 					<li><a href="https://plus.google.com/115140722630024401107" target="_blank"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
 				</ul>
-			</section> -->
+			</section>
 			<div id="copyright">
 				<span>&copy; <?php echo date("Y"); ?> Peak Website Services</span>
 				<br>
@@ -22,8 +45,13 @@
 	  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
 	  crossorigin="anonymous"></script>
 	  <script>window.jQuery || document.write('<script src="../node_modules/jquery/dist/jquery.min.js" async defer><\/script>')</script>
+
+		<!-- Font Awesome -->
+		<script src="https://use.fontawesome.com/ff486a1dc9.js"></script>
+
 		<!-- modernizr-custom -->
 		<script type="text/javascript" src="/_js/helper/modernizr-custom.js" async defer></script>
+
 		<!-- custom scripts -->
 		<script type="text/javascript" src="/_js/helper/bs-collapse.js"></script>
 		<script type="text/javascript" src="/_js/script.js" async defer></script>
@@ -31,9 +59,11 @@
 		<!-- If current page == contact page, load contact form javascript -->
 		<?php	echo $script = $_SERVER['PHP_SELF'] == '/contact.php' ? "<script src=\"_js/contact-form.js\"></script>" : "" ;?>
 
+		<!-- include Google Analytics -->
 		<?php include('gA.php'); ?>
 
 		<!-- NOTE: for Grunt Live Reload Server -->
 		<script src="//localhost:35729/livereload.js"></script>
+
 	</body>
 </html>
