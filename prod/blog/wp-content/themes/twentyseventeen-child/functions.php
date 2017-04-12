c34decf0d1756d6b57e54c85e7c54649d69624c8
David Gaskin <davidburkegaskin@gmail.com>
2017-04-12 11:51:16 -0700

    copy blog directory, functions.php file

---
+++ prod/blog/wp-content/themes/twentyseventeen-child/functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+	* Enqueue parent stylesheet
+	*/
+  function enqueue_parent_styles() {
+    $parent_style = 'twentyseventeen-style';
+
+  	// enqueue parent styles
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
+  	// enqueue child styles after parent
+    wp_enqueue_style( 'child-style',
+  		get_stylesheet_directory_uri() . '/style.css',
+  		array( $parent_style ),
+  		wp_get_theme()->get( 'Version' )
+  	);
+  }
+  add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+/**
+  * Load Google Analytics in the Footer
+  */
+  function load_googleAnalytics() {
+    include( '../_includes/gA.php');
+  }
+  add_action( 'wp_footer', 'load_googleAnalytics' );
+?>
