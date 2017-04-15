8198d7996ff2e08787788a7260cc711b2377ed22
David Gaskin <davidburkegaskin@gmail.com>
2017-04-15 10:47:33 -0700

    add style id to footer to fix blog style error

--- dev/_includes/footer.php
+++ dev/_includes/footer.php
@@ -1,4 +1,4 @@
-		<footer>
+		<footer id="pk-site-footer">
 			<?php require ('include-parts/footer-elements.php'); ?>
 			<?php require_once ('include-parts/footer-dependencies.php'); ?>
 			<?php include ('include-scripts/google-analytics.php'); ?>
