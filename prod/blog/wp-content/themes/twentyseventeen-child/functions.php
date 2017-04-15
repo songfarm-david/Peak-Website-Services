0fdca54ba092c4ccf5a8554b0ecadf43820de0a9
David Gaskin <davidburkegaskin@gmail.com>
2017-04-15 10:49:07 -0700

    add new build

--- prod/blog/wp-content/themes/twentyseventeen-child/functions.php
+++ prod/blog/wp-content/themes/twentyseventeen-child/functions.php
@@ -1,27 +1,39 @@
 <?php
 /**
-	* Enqueue parent stylesheet
-	*/
-  function enqueue_parent_styles() {
-    $parent_style = 'twentyseventeen-style';
-
-  	// enqueue parent styles
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-
-  	// enqueue child styles after parent
-    wp_enqueue_style( 'child-style',
-  		get_stylesheet_directory_uri() . '/style.css',
-  		array( $parent_style ),
-  		wp_get_theme()->get( 'Version' )
-  	);
-  }
-  add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+ * Functions.php for Peak Child Theme (twentyseventeen)
+ */
 
 /**
-  * Load Google Analytics in the Footer
-  */
-  function load_googleAnalytics() {
-    include( '../_includes/gA.php');
-  }
-  add_action( 'wp_footer', 'load_googleAnalytics' );
+ * Load Child CSS Styles
+ */
+function enqueue_parent_styles() {
+  $parent_style = 'twentyseventeen-style';
+
+	// enqueue parent styles
+  wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+
+	// enqueue child styles after parent
+  wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get( 'Version' )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+/**
+ * Load Additional scripts
+ */
+function load_custom_scripts() {
+  wp_enqueue_script( 'font-awesome', 'https://use.fontawesome.com/ff486a1dc9.js' );
+}
+add_action( 'wp_enqueue_scripts', 'load_custom_scripts' );
+
+/**
+ * Load Google Analytics in the Footer
+ */
+function load_googleAnalytics() {
+  include( '../_includes/include-parts/google-analytics.php' );
+}
+add_action( 'wp_footer', 'load_googleAnalytics' );
 ?>
