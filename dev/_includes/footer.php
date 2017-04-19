746fd412988a44545d2691dd9ae446c077353754
David Gaskin <davidburkegaskin@gmail.com>
2017-04-18 21:34:53 -0700

    add peak specific class to footer to differentiate from WP blog

--- dev/_includes/footer.php
+++ dev/_includes/footer.php
@@ -1,4 +1,4 @@
-		<footer id="pk-site-footer">
+		<footer class="pk-site-footer">
 			<?php require ('include-parts/footer-elements.php'); ?>
 			<?php require_once ('include-parts/footer-dependencies.php'); ?>
 			<?php include ('include-scripts/google-analytics.php'); ?>
