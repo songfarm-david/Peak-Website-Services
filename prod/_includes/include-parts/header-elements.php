d58b0b7567eb122696e086f4b31317fd162ca335
David Gaskin <davidburkegaskin@gmail.com>
2017-04-20 16:27:38 -0700

    move include-parts header, webFont loader to production build

---
+++ prod/_includes/include-parts/header-elements.php
@@ -0,0 +1,1 @@
+<a href="/" rel="home"><img class="brand-logo" src="/_images/logo/peak_logo.png" alt="Peak Websites Logo"> <span class="brand-title">Peak Websites</span> </a> <?php include_once( $_SERVER['DOCUMENT_ROOT'] . '/_includes/navigation.php') ?>
---
+++ prod/_includes/include-scripts/webFont-loader.php
@@ -0,0 +1,1 @@
+<script type="text/javascript">var WebFontConfig={google:{families:["Raleway:300,400,700,800,900"]},timeout:4e3};!function(){var t=document.createElement("script");t.src=("https:"==document.location.protocol?"https":"http")+"://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js",t.async="true",document.head.appendChild(t)}()</script>
