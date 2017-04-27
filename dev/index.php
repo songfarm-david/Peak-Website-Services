30d15ca672d91fd28a2c89d50b993b763ef6937f
David Gaskin <davidburkegaskin@gmail.com>
2017-04-26 21:48:31 -0700

    update landing page styles

--- dev/index.php
+++ dev/index.php
@@ -22,47 +22,68 @@ $canonical = "https://peakwebsites.ca/";
 
 		<!-- Insert new content here -->
 		<section>
-			<div>
-				<div id="intro-copy">
-					<h2>Online Success Made Easy</h2>
-					<p class="sub-header">From building you a website you love to making sure you're found online, Peak Websites makes being online simple.</p>
-					<!-- <p>With experience and expertise in everything from website development to search engine optimization, there really is no need to look anywhere else.</p> -->
-					<!-- <p>Best of all, you get personalized attention and support when you need it, all for a price that won't break the bank.</p> -->
-					<!-- Click to view some case studies -->
-					<!-- Read reviews from real businesses -->
-					<!-- <a href="#">Get Started Today</a> -->
-				</div>
-
-				<h3>Web Design &amp; Development</h3>
-				<p>Whether you need something simple or want a full eCommerce solution for your online business, Peak Websites can get you there.</p>
-				<p>We work with you to understand the needs of your website then deliver to you a highly effective and efficient solution that you can feel good about.</p>
-				<p>Should you find your website needs to evolve, we&rsquo;re there with you to meet the demands of your business.</p>
-				<a href="/website-services/website-development-services.php">Learn more about Website Development.</a>
-
-				<h3>Search Engine Optimization</h3>
-				<p>Competing with the multitude of websites online can be daunting but with the rights tool and guidance, being found online doesn't have to be out of reach.</p>
-				<p>Peak Websites specializes in ensuring that your website is optimized for the core keywords you want to be found for. We then continue to make strides in improving your visibility online by identifying core opportunities to strengthen your position online.</p>
-				<p>Peak Website is your best ally in taking charge of your results online.</p>
-				<a href="/website-services/website-optimization-services.php">Learn more about Website Optimization.</a>
-
-				<h3>Website Hosting</h3>
-				<p>Hosting your website on a trusted and reliable server is foundational to the success of your website.</p>
-				<p>At Peak, we host your website on state-of-the-art, industry-grade servers located in Canada and guaranteed to provide safe, secure coverage across North America and beyond.</p>
-				<p>Take advantage of Canadian web hosting and protect your data. You'll have peace of mind your website is safe and online 24/7/365.</p>
-				<a href="/website-services/website-hosting-services.php">Learn more about Website Hosting</a>
-
-				<h3>Website Maintenance</h3>
-				<p>You have more important things to do than monitoring the safety and security of your website.</p>
-				<p>We take responsibility for ensuring your website is up, online, free of errors and bugs, and working for you. We do this so you can focus on the more important things - like running your business.</p>
-				<p>At Peak, we treat your website like our own.</p>
-				<a href="/website-services/website-maintenance-services.php">Learn more about Website Maintenance</a>
-
-				<div id="cta-banner">
-					<!-- professional service/website, etc... -->
-					<p>Professional websites services made simple and affordable.</p>
-					<a href="/contact.php">Get Started Today</a>
+			<!-- <div> -->
+			<div class="panel color-1">
+				<div class="container">
+					<div id="intro-copy">
+						<h2>Online Success Made Simple</h2>
+						<p class="sub-header">From building you a website you love to making sure you're found online, Peak Websites takes the work out of having an online presence.</p>
+					</div>
 				</div>
 			</div>
+
+			<div class="panel">
+				<div class="container item">
+					<h3>Web Design &amp; Development</h3>
+					<p>Whether you need something simple or want a full eCommerce solution for your online business, Peak Websites can get you there.</p>
+					<p>We work with you to understand the needs of your website then deliver to you a highly effective and efficient solution that you can feel good about.</p>
+					<p>Should you find your website needs to evolve, we&rsquo;re there with you to meet the demands of your business.</p>
+					<a href="/website-services/website-development-services.php">Learn more about Website Development.</a>
+				</div>
+			</div>
+
+
+			<div class="panel color-2">
+				<div class="container item">
+					<h3>Search Engine Optimization</h3>
+					<p>Competing with the multitude of websites online can be daunting but with the rights tool and guidance, being found online doesn't have to be out of reach.</p>
+					<p>Peak Websites specializes in ensuring that your website is optimized for the core keywords you want to be found for. We then continue to make strides in improving your visibility online by identifying core opportunities to strengthen your position online.</p>
+					<p>Peak Website is your best ally in taking charge of your results online.</p>
+					<a href="/website-services/website-optimization-services.php">Learn more about Website Optimization.</a>
+				</div>
+			</div>
+
+			<div class="panel">
+				<div class="container item">
+					<h3>Website Hosting</h3>
+					<p>Hosting your website on a trusted and reliable server is foundational to the success of your website.</p>
+					<p>At Peak, we host your website on state-of-the-art, industry-grade servers located in Canada and guaranteed to provide safe, secure coverage across North America and beyond.</p>
+					<p>Take advantage of Canadian web hosting and protect your data. You'll have peace of mind your website is safe and online 24/7/365.</p>
+					<a href="/website-services/website-hosting-services.php">Learn more about Website Hosting</a>
+				</div>
+			</div>
+
+
+			<div class="panel color-2">
+				<div class="container item">
+					<h3>Website Maintenance</h3>
+					<p>You have more important things to do than monitoring the safety and security of your website.</p>
+					<p>We take responsibility for ensuring your website is up, online, free of errors and bugs, and working for you. We do this so you can focus on the more important things - like running your business.</p>
+					<p>At Peak, we treat your website like our own.</p>
+					<a href="/website-services/website-maintenance-services.php">Learn more about Website Maintenance</a>
+				</div>
+			</div>
+
+			<div class="panel">
+				<div class="container">
+					<div id="cta-banner">
+						<p>Take the work out of having an online presence</p>
+						<a href="/contact.php">Get Started Today</a>
+					</div>
+				</div>
+			</div>
+
+			<!-- </div> -->
 		</section>
 
 	</main>
