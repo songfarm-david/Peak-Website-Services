ed7e5ed3ae1fc7d16495645ae2d55d5123ad492d
David Gaskin <davidburkegaskin@gmail.com>
2017-05-25 00:06:20 -0700

    add new copy to website development service page

--- dev/website-services/website-development-services.php
+++ dev/website-services/website-development-services.php
@@ -18,32 +18,63 @@ $activePage = basename($_SERVER['PHP_SELF']);
 		</section><!--/ end of service -->
 
 		<section class="service-types">
-			<h3>Basic Website</h3>
-			<p>A basic website consists of 5 pages:</p>
-			<ul>
-				<li>Homepage</li>
-				<li>About page</li>
-				<li>Products/Services page</li>
-				<li>Contact Page</li>
-				<li>One additional page (like gallery, blog, etc..)</li>
-			</ul>
-			<p>A basic website allows for the ability to edit and add content easily to your website yourself, for instance, if you want have a blog or post updates as you go.</p>
-			<p>The cost of a basic website is <i>$599</i> with each additional pages costing $99.</p>
+			<h3>Website Design and Development</h3>
+			<p>See our selection of website options and development services:</p>
+			<section>
+				<h4>Starter WordPress Website</h4>
+				<p>A starter WordPress website consists of 5 pages:</p>
+				<ul>
+					<li>Homepage</li>
+					<li>About page</li>
+					<li>Products/Services page</li>
+					<li>Contact Page</li>
+					<li>One additional page (Gallery, Blog, etc..)</li>
+				</ul>
+				<p>This is a great option for anyone looking to have a strong presence online.</p>
+				<p>WordPress allows for easy do-it-yourself content updating, so that if you want to add new content to your website, such as a blog post, for example, this is easily achieved. <em>No coding skills necessary</em>. We will teach you how to do it at <em>no extra charge</em>.</p>
+				<p>If you want to expand your website later on, WordPress is a great option for adding more complex integrations like an email sign-up form, pop-ups, and online payment processing.</p>
+				<p class="cost">The cost of a basic website is $599 with each additional pages costing $99.</p>
+				<!-- <p>For simple and affordable, WordPress is a great option!</p> -->
+			</section>
+			<section>
+				<h4>Wix, Squarespace, or Weebly Website</h4>
+				<p>For those who want more control, website builders like Wix, Squarespace, and Weebly make it possible for those without website coding experience to build, design, and create content for a website.</p>
+				<p>However, you may need to help setting it up and getting it online, or to create pages and design your website.</p>
+				<p>Depending on your needs, we can help get you started with one of these website builders for $199 including web hosting.</p>
+				<p><a href="/contact.php">Contact Us</a> for a full quote.</p>
+			</section>
+			<section>
+				<h4>HTML5 Website</h4>
+				<p>A website built with HTML5 uses the most modern coding languages, is more secure than WordPress and guarantees you something that is uniquely yours.</p>
+				<p>Our design team can deliver a custom look and fully-personalized website that will make your business and brand stand out!</p>
+				<p>HTML5 websites are fully responsive to adjust for mobile screens, are fast and efficient to load, and will set you apart online.</p>
+				<p class="cost">The cost for an HTML5 website starts at $1,000 but varies depending on the size and scope of the project. Please <a href="/contact.php">Contact Us</a> for a quote.</p>
+			</section>
+			<section>
+				<h4>Custom Development Work</h4>
+				<p>Sometimes you may need to add a new feature to your website, or to update certain aspects of your site to meet the demands of your business.</p>
+				<p>Our developers can assist you in making the proper integrations and provide you with the solution you need.</p>
+				<p class="cost">Development cost for Peak clients is $35/hr ($55/hr for non-clients).</p>
+			</section>
 		</section>
 
+
+
+
+
 		<!-- NOTE: this section might be too fluffy -->
-		<!-- <section class="section-temp">
+		<section class="section-temp">
 			<h3>About Our Websites</h3>
 			<p>When we build a website, we have a few objectives we strive to meet:</p>
 			<ol>
 				<li>To clearly and robustly meet the demand of the problem; that is to say, solving <em>your unique online business challenge</em> in the most economical and elegant way.</li>
 				<li>To code in a clean, compact, and <a href="wai-aria">inclusive</a> way, in-step with current best-practices and to create an application that not only looks beautiful but <em>works</em> on a vast array of viewing devices</li>
-				<li>To remain dedicated to ensuring the continued service of our websites, from initial release through to its present state</li>
+				<li>To remain dedicated to ensuring the continued functionality of our websites, from initial release through to its present state</li>
 			</ol>
 			<br>
-			<a href="/about.php">Learn more about Peak</a>
+			<a href="/about.php">Learn More About Us</a>
 			<a href="/portfolio/">See our Portfolio</a>
-		</section> -->
+		</section>
 
 		<p class="call-to-action">Let&rsquo;s get the ball rolling. <a href="/contact.php">Contact Us Today</a> for a no-pressure chat about your online development needs.</p>
 
