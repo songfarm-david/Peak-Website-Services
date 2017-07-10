40bdb041c59f449b8e33e0238be0dd5c2564b9f6
David Gaskin <davidburkegaskin@gmail.com>
2017-07-10 12:23:20 -0700

    new prod build

---
+++ prod/_includes/include-scripts/schema.php
@@ -0,0 +1,12 @@
+<script type="application/ld+json">{
+  "@context": "http://schema.org/",
+  "@type": "aggregateRating",
+  "itemReviewed": {
+    "@type": "localBusiness",
+    "image": "https://peakwebsites.ca/_images/air-triangulated.jpg",
+    "name": "Peak Websites",
+    "telephone": "778-587-9220"
+   },
+  "ratingCount": "8",
+  "ratingValue": "4.8"
+}</script>
