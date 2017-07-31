7e6b5e86cd65bcc1cc26889db60321df3e136458
David Gaskin <davidburkegaskin@gmail.com>
2017-07-31 16:20:48 -0700

    update testimonial design/styles; update website maintenance services page

--- dev/website-services/website-maintenance-services.php
+++ dev/website-services/website-maintenance-services.php
@@ -31,14 +31,14 @@ $activePage = basename($_SERVER['PHP_SELF']);
 			<section class="panel">
 				<div class="panel-container">
 					<div class="service-unit">
-						<p>Your website is pivotal to your online success. It's one of the first places a potential customer goes when deciding whether they want to trust and spend their money on you, where they go to get a sense of your credibility and what you're all about.</p>
-						<p><em>Don't let them down.</em></p>
+						<p>Your website is pivotal to your online success. It's one of the first places a potential customer to get a sense of your credibility and what your business is all about.</p>
 						<p>Your website might already have a lot going for it but it's important that it doesn't get stale. Worse yet, that it's hacked and is no longer working for your business.</p>
-						<p>Without the right experience and know-how, your website is at risk.</p>
-						<p>Don't go it alone.</p>
-						<p>We understand that your website is one of your most important assets online and work with you 24/7 to ensure your website is secure, up-to-date, and working to serve your business.</p>
-						<p>You can count on us to:</p>
-						<ul>
+						<p>Without the right experience and know-how, you're running blind.</p>
+						<p><strong>Don't go it alone.</strong></p>
+						<p>We understand that your website is one of your most important assets online.</p>
+						<p>We maintain your website month after month, ensuring it stays secure and up-to-date, and are standing by to make changes to it when you need a fix or when inspiration strikes.</p>
+						<ul class="small-list">
+							<p>You can count on us to:</p>
 							<li>Be responsible for all the technical details (domain registrations, hosting services, logins and passwords)</li>
 							<li>Answer any technical questions you may have</li>
 							<li>Provide professional, on-time service</li>
@@ -46,48 +46,101 @@ $activePage = basename($_SERVER['PHP_SELF']);
 						</ul>
 						<br>
 						<div class="button-container">
-							<a href="#maintenance-pricing" class="service-button">See Website Maintenance Service Plans</a>
+							<a href="#maintenance-pricing" class="service-button button-wireframe">See Website Maintenance Service Plans</a>
 						</div>
 					</div>
 				</div>
 			</section>
 
-			<section class="panel color-2 feature-grid">
+			<section class="panel color-3 feature-grid">
 				<div class="panel-container">
 					<div class="service-unit row">
-						<h3>Website Maintenance Features</h3>
-						<p>Included in all our Website Maintenance Plans:</p>
-						<div class="col col-4">
-							<h4>24/7 Security</h4>
+						<h3 class="title">Website Maintenance Features</h3>
+						<p class="subtitle">Included in all our Website Maintenance Plans:</p>
+						<div class="col col-4 feature">
+							<h4 class="feature-header">24/7 Security</h4>
 							<p>We regularly monitor and ensure your website is working as expected and protected against online threats.</p>
 						</div>
-						<div class="col col-4">
-							<h4>Daily Backups</h4>
+						<div class="col col-4 feature">
+							<h4 class="feature-header">Daily Backups</h4>
 							<p>Have peace of mind knowing that your website is backed up in case of data loss or malicious intrusions.</p>
 						</div>
-						<div class="col col-4">
-							<h4>Unlimited Content Updates</h4>
-							<p>We regularly monitor and ensure your website is working as expected and protected against online threats.</p>
+						<div class="col col-4 feature">
+							<h4 class="feature-header">Unlimited Content Updates</h4>
+							<p></p>
 						</div>
 					</div>
 					<div class="service-unit row">
-						<div class="col col-4">
-							<h4>Personal Service</h4>
+						<div class="col col-4 feature">
+							<h4 class="feature-header">Personal Service</h4>
 							<p>We regularly monitor and ensure your website is working as expected and protected against online threats.</p>
 						</div>
-						<div class="col col-4">
-							<h4>No Contracts</h4>
+						<div class="col col-4 feature">
+							<h4 class="feature-header">No Contracts</h4>
 							<p>Our service plans are billed month-to-month so come and go as you please; upgrade or downgrade your service as required. No contracts involved.</p>
 						</div>
-						<div class="col col-4">
-							<h4>Quick Turnaround</h4>
+						<div class="col col-4 feature">
+							<h4 class="feature-header">Quick Turnaround</h4>
 							<p>No more waiting for weeks to get something changed on your website. With us, most updates happen in 1-2 days.</p>
 						</div>
 					</div>
 				</div>
 			</section>
 
-			
+			<section class="cta-container">
+				<h3 class="hide">Contact Us</h3>
+				<p>Ready to take the next step?</p>
+				<p class="cta"><a href="/contact.php">Get Started Today</a></p>
+			</section>
+
+
+			<!-- put Testimonial here -->
+			<article class="panel testimonials-panel">
+				<div class="container">
+					<div class="panel-inner">
+						<h3 class="at-only">Testimonials</h3>
+						<img class="testimonial-image" src="/_images/testimonies/nancy-crites.png" alt="A headshot of Nancy Crites">
+						<p class="testimonial-text">This is just filler text. David did such a great job! I would definitely recommend him to anyone else!</p>
+					</div>
+				</div>
+			</article>
+
+
+			<!-- Website Maintenance Questions -->
+			<section class="panel"><!-- need a module or identifying name for this unit -->
+				<div class="container">
+					<div class="panel-inner">
+						<h4>Questions About Website Maintenance</h4>
+						<p class="question">How does Website Maintenance work?</p>
+						<p>When you sign up for one of our fixed <a href="#">monthly service packages</a>, you are guaranteed a set portion of hours dedicated to adding or updating content on your website. This is on on top of the already included <a href="#">features</a>.</p>
+						<p>When you have the need to change or add content to your website, simply send us an email detailing what you want done and we’ll take care of the rest.</p>
+						<p>If you can’t find anything you want done that month, the hours included in your package will be used to address other aspects of your website, like SEO, or technical performance.</p>
+						<p>Each month, you’ll get a detailed report about your website’s traffic, it’s performance, the work we did, and what we have in mind for the future.</p>
+						<p>Anyway you slice it, you’re investment is put to good use.</p>
+
+						<p class="question">How does Website Maintenance work?</p>
+						<p>A typical service request will be completed within 48 hours and often within 12 - 24 hours. In the event of larger undertakings, updates may take a few days.</p>
+						<p>In any case, the work will get done when we say it will.</p>
+
+						<p class="question">Do I have to sign a contract?</p>
+						<p>No. All of our website maintenance packages are paid month-to-month. You are free to cancel at any time as well as upgrade or downgrade your service as you see fit.</p>
+
+						<p class="question">What can I expect in a Website Health and Traffic Report?</p>
+						<p>A Website Health and Traffic Report is a great way to let you know how your website is performing both on the customer-facing side, your web traffic, and in terms of the technical and performance-related front.</p>
+						<p>We want you to understand how your website is doing so that, together, we can make better informed decisions in how we service and maintain your website for optimal results.</p>
+						<p>Each month, we will give you insights into who’s coming to your website and how they’re behaving once they’re there. We’ll also tell you what search terms they’re using to find you online.</p>
+						<p>In addition to that, we’ll let you know how your website is performing from a technical perspective so there are no surprises down the road.</p>
+						<p>In our experience, there is no better way of making progress than measuring current success, and our Website Health and Traffic Reports do just that!</p>
+
+						<p class="question">Do you really offer Unlimited Content Updates?</p>
+						<p>Well, we are human after all. But yes, there isn’t a limit to what you can request.</p>
+						<p>Of course, each package offers different pricing structures and benefits should you go over your monthly allotted time.</p>
+						<p>If you expect you will need more time regularly, contact us for a custom quote.</p>
+
+					</div>
+				</div>
+			</section>
+			<!--#end Website Maintenance Questions -->
 
 			<article class="pricing" id="maintenance-pricing">
 				<div class="pricing-outer">
